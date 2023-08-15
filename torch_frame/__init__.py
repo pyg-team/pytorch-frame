3649816b51effbaaddb949847a97b6665846606b
Weihua Hu <weihua916@gmail.com>
2023-08-15 03:02:27 +0000

    update

--- torch_frame/__init__.py
+++ torch_frame/__init__.py
@@ -1,5 +1,6 @@
 from .stype import stype, numerical, categorical, unsupported
 from .data.tensor_frame import TensorFrame
+from .data.embedding_frame import EmbeddingFrame
 
 __version__ = '0.1.0'
 
@@ -9,5 +10,6 @@ __all__ = [
     'categorical',
     'unsupported',
     'TensorFrame',
+    'EmbeddingFrame',
     '__version__',
 ]
