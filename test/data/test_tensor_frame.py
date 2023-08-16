791234cecf1faa152d2733044d4395d0d98bdc4c
Weihua Hu <weihua916@gmail.com>
2023-08-16 11:36:00 -0700

    update

--- test/data/test_tensor_frame.py
+++ test/data/test_tensor_frame.py
@@ -6,9 +6,10 @@ import torch_frame
 
 
 def test_tensor_frame_basics():
-    x_dict = {}
-    x_dict[torch_frame.categorical] = torch.randint(0, 3, size=(10, 3))
-    x_dict[torch_frame.numerical] = torch.randn(size=(10, 2))
+    x_dict = {
+        torch_frame.categorical: torch.randint(0, 3, size=(10, 3)),
+        torch_frame.numerical: torch.randn(size=(10, 2)),
+    }
     col_names_dict = {}
     col_names_dict[torch_frame.categorical] = ['a', 'b', 'c']
     col_names_dict[torch_frame.numerical] = ['x', 'y']
@@ -28,23 +29,23 @@ def test_tensor_frame_error():
 
     # Wrong number of channels
     x_dict[torch_frame.categorical] = torch.randint(0, 3, size=(10, 2, 3))
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError, match='not a 2-dimensional tensor'):
         TensorFrame(x_dict=x_dict, col_names_dict=col_names_dict, y=y)
     x_dict[torch_frame.categorical] = torch.randint(0, 3, size=(10, 3))
 
     # Mis-alignment of the col_names and the number of columns in x_dict
     x_dict[torch_frame.categorical] = torch.randint(0, 3, size=(10, 4))
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError, match='not align with'):
         TensorFrame(x_dict=x_dict, col_names_dict=col_names_dict, y=y)
     x_dict[torch_frame.categorical] = torch.randint(0, 3, size=(10, 3))
 
     # Mis-alignment of the lengths within x_dict
     x_dict[torch_frame.categorical] = torch.randint(0, 3, size=(11, 3))
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError, match='not aligned'):
         TensorFrame(x_dict=x_dict, col_names_dict=col_names_dict, y=y)
     x_dict[torch_frame.categorical] = torch.randint(0, 3, size=(10, 3))
 
     # Mis-alignment between the lengths of x_dict and y
     y = torch.randn(11)
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError, match='not aligned'):
         TensorFrame(x_dict=x_dict, col_names_dict=col_names_dict, y=y)
