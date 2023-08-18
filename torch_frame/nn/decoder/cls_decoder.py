472d2d125cc342906ce3c6601216f2c04d2ae3b9
Weihua Hu <weihua916@gmail.com>
2023-08-17 18:34:04 -0700

    update

--- torch_frame/nn/decoder/cls_decoder.py
+++ torch_frame/nn/decoder/cls_decoder.py
@@ -4,6 +4,12 @@ from torch_frame.nn.decoder import Decoder
 
 
 class CLSDecoder(Decoder):
+    """CLS-token decoder used by FT-Transformer model in
+    https://arxiv.org/abs/2106.11959
+
+    This module simply extracts the first column embeddings (corresponding to
+    the CLS token) of input x output by `torch_frame.nn.conv.FTTransformerConv`
+    """
     def __init__(self):
         super().__init__()
 
