f04bf917fb394e20b55121c8cb8423a4492fe955
Weihua Hu <weihua916@gmail.com>
2023-08-23 11:08:43 -0700

    update

--- test/nn/decoder/test_trompt_decoder.py
+++ test/nn/decoder/test_trompt_decoder.py
@@ -7,7 +7,7 @@ def test_trompt_decoder():
     batch_size = 10
     num_prompts = 2
     in_channels = 8
-    out_channels = 2
+    out_channels = 1
     x_prompt = torch.randn(batch_size, num_prompts, in_channels)
     decoder = TromptDecoder(in_channels, out_channels, num_prompts)
     y = decoder(x_prompt)
