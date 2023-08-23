da703ddb5008cc9fc8425df1f4df5c31a3815f3c
Weihua Hu <weihua916@gmail.com>
2023-08-23 10:55:41 -0700

    Add `TromptConv` (#23)
    Paper: https://arxiv.org/pdf/2305.18446.pdf
    
    See Figure 3 for the architecture.

--- torch_frame/nn/conv/__init__.py
+++ torch_frame/nn/conv/__init__.py
@@ -1,7 +1,9 @@
 from .table_conv import TableConv
 from .ft_transformer import FTTransformer
+from .trompt_conv import TromptConv
 
 __all__ = classes = [
     'TableConv',
     'FTTransformer',
+    'TromptConv',
 ]
