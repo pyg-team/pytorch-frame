0606a67481c0a9c614debd47152d4f309edc9065
Yiwen Yuan <yiwen@kumo.ai>
2023-08-26 05:00:31 +0000

    fix

--- test/nn/conv/test_excelformer_conv.py
+++ test/nn/conv/test_excelformer_conv.py
@@ -1,9 +1,9 @@
 import torch
+import sys
 
 from torch_frame.nn import ExcelFormerConv
 
-
-def test_trompt_conv():
+def test_excelformer_conv():
     batch_size = 10
     channels = 8
     num_cols = 15
@@ -12,5 +12,5 @@ def test_trompt_conv():
     x = torch.randn(size=(batch_size, num_cols, channels))
 
     conv = ExcelFormerConv(channels=8, num_cols=num_cols, num_heads=num_heads)
-    x_prompt = conv(x)
-    assert x_prompt.shape == (batch_size, num_cols, channels)
+    x_out = conv(x)
+    assert x_out.shape == (batch_size, num_cols, channels)
