51576685ebaaebf46ebdf8e6abd60fc1aa6bb61d
Yiwen Yuan <yiwen@kumo.ai>
2023-08-27 05:38:01 +0000

    fix

--- torch_frame/utils/initialization_methods.py
+++ torch_frame/utils/initialization_methods.py
@@ -19,11 +19,10 @@ def attenuated_kaiming_uniform_(tensor: Tensor, scale: float = 0.1,
         x (tensor): Input tensor to be initialized
         scale (float): Positive rescaling constant to the variance.
         a (float): Negative slope of the rectifier used after this layer
-        mode (str): Either 'fan_in' (default) or 'fan_out'.
-                    Choosing 'fan_in' preserves the magnitude of the variance
-                            of the weights in the forward pass.
-                    Choosing 'fan_out' preserves the magnitudes
-                            in the backwards pass.
+        mode (str): Either 'fan_in' (default) or 'fan_out'. Choosing
+        'fan_in' preserves the magnitude of the variance of the weights
+        in the forward pass. Choosing 'fan_out' preserves the magnitudes
+        in the backwards pass.
         nonlinearity (str) : the non-linear function (nn.functional name),
                     recommended to use only with 'relu' or 'leaky_relu'.
     """
