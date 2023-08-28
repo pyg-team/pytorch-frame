d11a86152bed067bfbc8f51c2046919a90575c3f
Yiwen Yuan <yiwen@kumo.ai>
2023-08-28 18:48:15 +0000

    fix

--- torch_frame/nn/conv/excelformer_conv.py
+++ torch_frame/nn/conv/excelformer_conv.py
@@ -10,11 +10,9 @@ from torch_frame.nn.conv import TableConv
 from torch_frame.utils import attenuated_kaiming_uniform_
 
 
-def attenuated_initialization(m):
-    classname = m.__class__.__name__
-    if classname.find('Linear') != -1:
-        attenuated_kaiming_uniform_(m.weight)
-        zeros_(m.bias)
+def init_attenuated(linear: Linear):
+    attenuated_kaiming_uniform_(linear.weight)
+    zeros_(linear.bias)
 
 
 class AiuM(Module):
@@ -29,10 +27,11 @@ class AiuM(Module):
         self.lin_1 = Linear(channels, channels)
         self.lin_2 = Linear(channels, channels)
         self.dropout = Dropout(dropout)
+        self.reset_parameters()
 
     def reset_parameters(self):
-        self.lin_1.apply(attenuated_initialization)
-        self.lin_2.apply(attenuated_initialization)
+        init_attenuated(self.lin_1)
+        init_attenuated(self.lin_2)
 
     def forward(self, x):
         x = self.dropout(F.tanh(self.lin_1(x)) * (self.lin_2(x)))
@@ -57,12 +56,13 @@ class DiaM(Module):
         self.lin_out = Linear(channels, channels) if num_heads > 1 else None
         self.num_heads = num_heads
         self.dropout = Dropout(dropout)
+        self.reset_parameters()
 
     def reset_parameters(self):
         for lin in [self.lin_q, self.lin_k, self.lin_v]:
-            lin.apply(attenuated_initialization)
+            init_attenuated(lin)
         if self.lin_out is not None:
-            self.lin_out.apply(attenuated_initialization)
+            init_attenuated(self.lin_out)
 
     def _reshape(self, x: Tensor) -> Tensor:
         B, num_cols, channels = x.shape
@@ -72,7 +72,7 @@ class DiaM(Module):
                                             2).reshape(B * self.num_heads,
                                                        num_cols, d_head))
 
-    def get_attention_mask(self, input_shape, device):
+    def get_attention_mask(self, input_shape, device: torch.device):
         B, _, channels = input_shape
         seq_ids = torch.arange(channels, device=device)
         attention_mask = seq_ids[None, None, :].repeat(B, channels,
@@ -84,8 +84,6 @@ class DiaM(Module):
     def forward(self, x: Tensor) -> Tensor:
         B, num_cols, _ = x.shape
         Q, K, V = self.lin_q(x), self.lin_k(x), self.lin_v(x)
-        for tensor in [Q, K, V]:
-            assert tensor.shape[-1] % self.num_heads == 0
         d_heads = V.shape[-1] // self.num_heads
         Q = self._reshape(Q)
         K = self._reshape(K)
@@ -117,9 +115,14 @@ class ExcelFormerConv(TableConv):
         initialization (str): 'kaiming' or 'xavier' for attenuated
         kaiming or attenuated xavier initialization
     """
-    def __init__(self, channels: int, num_heads: int, diam_dropout: float = 0,
-                 aium_dropout: float = 0, residual_dropout: float = 0,
-                 initialization: str = 'kaiming') -> None:
+    def __init__(
+        self,
+        channels: int,
+        num_heads: int,
+        diam_dropout: float = 0.0,
+        aium_dropout: float = 0.0,
+        residual_dropout: float = 0.0,
+    ) -> None:
 
         super().__init__()
         self.norm_1 = LayerNorm(channels)
@@ -129,10 +132,10 @@ class ExcelFormerConv(TableConv):
         self.residual_dropout = residual_dropout
 
     def reset_parameters(self):
-        self.DiaM.apply(attenuated_initialization)
-        self.AiuM.apply(attenuated_initialization)
-        self.norm_1.apply(attenuated_initialization)
-        self.norm_2.apply(attenuated_initialization)
+        self.DiaM.reset_parameters()
+        self.AiuM.reset_parameters()
+        init_attenuated(self.norm_1)
+        init_attenuated(self.norm_2)
 
     def _start_residual(self, x):
         x_residual = x
