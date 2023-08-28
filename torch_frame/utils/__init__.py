5399358babd5de7d1dbd54e8e9c85218d7a6d0c4
Yiwen Yuan <yyuanlisette@gmail.com>
2023-08-28 11:32:59 -0700

    Update torch_frame/utils/__init__.py
    Co-authored-by: Matthias Fey <matthias.fey@tu-dortmund.de>

--- torch_frame/utils/__init__.py
+++ torch_frame/utils/__init__.py
@@ -2,5 +2,5 @@ from .initialization_methods import (attenuated_kaiming_uniform_,
                                      attenuated_xavier_uniform_)
 
 __all__ = functions = [
-    'attenuated_kaiming_uniform_', 'attenuated_xavier_uniform_'
+    'attenuated_kaiming_uniform_', 'attenuated_xavier_uniform_',
 ]
