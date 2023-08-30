2b854a408939ad29de9c6f716777a3ed4b1b4c68
Weihua Hu <weihua916@gmail.com>
2023-08-30 00:54:42 +0000

    update

--- torch_frame/datasets/yandex.py
+++ torch_frame/datasets/yandex.py
@@ -8,25 +8,24 @@ import pandas as pd
 import torch_frame
 
 
-def load_dataset(path: str) -> Dict[str, np.ndarray]:
-    r"""Load a dataset from a ZIP file.
+def load_numpy_dict(path: str) -> Dict[str, np.ndarray]:
+    r"""Load numpy files from a ZIP file.
 
     Args:
-        path (str): The file path to the ZIP file containing some .npy files
-            that store the dataset.
+        path (str): A path to the ZIP file containing .npy files.
 
     Returns:
-        dataset (Dict[str, np.ndarray]): A dictionary that maps the name of
+        numpy_dict (Dict[str, np.ndarray]): A dictionary that maps the name of
             .npy file to the loaded numpy array.
     """
-    dataset: Dict[str, np.ndarray] = {}
+    numpy_dict: Dict[str, np.ndarray] = {}
     with zipfile.ZipFile(path, 'r') as zip_ref:
         for file_name in zip_ref.namelist():
             if file_name.endswith('.npy'):
                 with zip_ref.open(file_name) as f:
                     array_name = osp.basename(file_name).replace('.npy', '')
-                    dataset[array_name] = np.load(f, allow_pickle=True)
-    return dataset
+                    numpy_dict[array_name] = np.load(f, allow_pickle=True)
+    return numpy_dict
 
 
 def get_df_and_col_to_stype(
@@ -34,28 +33,28 @@ def get_df_and_col_to_stype(
     r"""Get DataFrame and :obj:`col_to_stype` from a ZIP file.
 
     Args:
-        zip_file_path (str): The file path of the ZIP file containing
-            the dataset.
+        zip_file_path (str): A path of the ZIP file containing .npy files.
 
     Returns:
         df (DataFrame): DataFrame containing train/val/test rows.
         col_to_stype (Dict[str, torch_frame.stype]). A dictionary mapping
             column names to their respective semantic types.
     """
-    dataset = load_dataset(zip_file_path)
+    numpy_dict = load_numpy_dict(zip_file_path)
     dataframes: List[pd.DataFrame] = []
     col_to_stype: Dict[str, torch_frame.stype] = {}
 
     for split in ['train', 'val', 'test']:
-        categorical_features = dataset.get(f'C_{split}', None)
-        numerical_features = dataset.get(f'N_{split}', None)
-        labels = dataset[f'y_{split}']
-        assert (categorical_features is None) and (numerical_features is None)
+        categorical_features = numpy_dict.get(f'C_{split}', None)
+        numerical_features = numpy_dict.get(f'N_{split}', None)
+        labels = numpy_dict[f'y_{split}']
+        assert not ((categorical_features is None) and
+                    (numerical_features is None))
 
-        merged_features: Optional[np.ndarray] = None
+        features: Optional[np.ndarray] = None
         if (categorical_features is not None) and (numerical_features
                                                    is not None):
-            merged_features = np.concatenate(
+            features = np.concatenate(
                 [categorical_features, numerical_features], axis=1)
             c_col_names = [
                 f'C_feature_{i}' for i in range(categorical_features.shape[1])
@@ -70,20 +69,19 @@ def get_df_and_col_to_stype(
             for name in n_col_names:
                 col_to_stype[name] = torch_frame.numerical
         else:
-            # if the dataset contains only categorical or numerical features
-            merged_features = (categorical_features if categorical_features
-                               is not None else numerical_features)
+            # if the numpy_dict contains only categorical or numerical features
+            features = (categorical_features if categorical_features
+                        is not None else numerical_features)
             feature_type = 'C' if categorical_features is not None else 'N'
             col_names = [
-                f'{feature_type}_feature_{i}'
-                for i in range(merged_features.shape[1])
+                f'{feature_type}_feature_{i}' for i in range(features.shape[1])
             ]
 
             for name in col_names:
                 col_to_stype[name] = (torch_frame.categorical if feature_type
                                       == 'C' else torch_frame.numerical)
-        assert merged_features is not None
-        df = pd.DataFrame(merged_features, columns=col_names)
+        assert features is not None
+        df = pd.DataFrame(features, columns=col_names)
         df['label'] = labels
         # Stores the split information in "split" column.
         df['split'] = split
@@ -113,4 +111,6 @@ class Yandex(torch_frame.data.Dataset):
             col_to_stype['label'] = torch_frame.numerical
         else:
             col_to_stype['label'] = torch_frame.categorical
+        import pdb
+        pdb.set_trace()
         super().__init__(df, col_to_stype, target_col='label')
