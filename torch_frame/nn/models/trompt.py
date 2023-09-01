447b9056b45f621f64d4aad04c686fde25c0ca8f
Weihua Hu <weihua916@gmail.com>
2023-09-01 21:02:00 +0000

    make trompt model to take tf as input

--- torch_frame/nn/models/trompt.py
+++ torch_frame/nn/models/trompt.py
@@ -1,8 +1,18 @@
+from typing import Any, Dict, List
+
 import torch
 from torch import Tensor
-from torch.nn import Module, ModuleList, Parameter
+from torch.nn import Module, ModuleList, Parameter, ReLU
 from torch.nn.modules.module import Module
 
+import torch_frame
+from torch_frame import TensorFrame, stype
+from torch_frame.data.stats import StatType
+from torch_frame.nn import (
+    EmbeddingEncoder,
+    LinearEncoder,
+    StypeWiseFeatureEncoder,
+)
 from torch_frame.nn.conv import TromptConv
 from torch_frame.nn.decoder import TromptDecoder
 
@@ -11,33 +21,54 @@ class Trompt(Module):
     r"""The Trompt model introduced in https://arxiv.org/abs/2305.18446
 
     Args:
-        in_channels (int): Input channel dimensionality
+        channels (int): Hidden channel dimensionality
         out_channels (int): Output channels dimensionality
         num_cols (int): Number of columns
         num_prompts (int): Number of prompt columns.
         num_layers (int, optional): Number of :class:`TromptConv` layers.
             (default: :obj:`6`)
+        col_stats (Dict[str, Dict[StatType, Any]]): A dictionary that maps
+            column name into stats. Available as :obj:`dataset.col_stats`.
+        col_names_dict (Dict[torch_frame.stype, List[str]]): A dictionary that
+            maps stype to a list of column names. The column names are sorted
+            based on the ordering that appear in :obj:`tensor_frame.x_dict`.
+            Available as :obj:`tensor_frame.col_names_dict`.
     """
     def __init__(
         self,
-        in_channels: int,
+        channels: int,
         out_channels: int,
         num_cols: int,
         num_prompts: int,
-        num_layers: int = 6,
+        num_layers: int,
+        # kwargs for encoder
+        col_stats: Dict[str, Dict[StatType, Any]],
+        col_names_dict: Dict[torch_frame.stype, List[str]],
     ):
         super().__init__()
-        self.in_channels = in_channels
+        self.channels = channels
         self.out_channels = out_channels
         self.num_cols = num_cols
+        self.num_layers = num_layers
 
-        self.x_prompt = Parameter(torch.empty(num_prompts, in_channels))
-        self.trompt_convs = ModuleList([
-            TromptConv(in_channels, num_cols, num_prompts)
-            for _ in range(num_layers)
-        ])
+        self.x_prompt = Parameter(torch.empty(num_prompts, channels))
+        self.encoders = ModuleList()
+        self.trompt_convs = ModuleList()
+        for _ in range(num_layers):
+            self.encoders.append(
+                StypeWiseFeatureEncoder(
+                    out_channels=channels,
+                    col_stats=col_stats,
+                    col_names_dict=col_names_dict,
+                    stype_encoder_dict={
+                        stype.categorical: EmbeddingEncoder(),
+                        stype.numerical: LinearEncoder(post_module=ReLU()),
+                    },
+                ))
+            self.trompt_convs.append(
+                TromptConv(channels, num_cols, num_prompts))
         # Decoder is shared across layers.
-        self.trompt_decoder = TromptDecoder(in_channels, out_channels,
+        self.trompt_decoder = TromptDecoder(channels, out_channels,
                                             num_prompts)
         self.reset_parameters()
 
@@ -47,26 +78,26 @@ class Trompt(Module):
             trompt_conv.reset_parameters()
         self.trompt_decoder.reset_parameters()
 
-    def forward(self, x: Tensor) -> Tensor:
+    def forward(self, tf: TensorFrame) -> Tensor:
         r"""Transforming :obj:`x` into a series of output predictions at each
         layer.
 
         Args:
-            x (Tensor): Input column-wise tensor of shape
-                [batch_size, num_cols, in_channels]
+            tf (TensorFrame): Input TensorFrame
 
         Returns:
             stacked_out (Tensor): Output predictions stacked across layers. The
                 shape is [batch_size, num_layers, out_channels].
         """
-
-        batch_size = len(x)
+        batch_size = len(tf)
         outs = []
-        # [batch_size, num_prompts, in_channels]
+        # [batch_size, num_prompts, channels]
         x_prompt = self.x_prompt.repeat(batch_size, 1, 1)
-        for trompt_conv in self.trompt_convs:
-            # [batch_size, num_prompts, in_channels]
-            x_prompt = trompt_conv(x, x_prompt)
+        for i in range(self.num_layers):
+            # [batch_size, num_cols, channels]
+            x, _ = self.encoders[i](tf)
+            # [batch_size, num_prompts, channels]
+            x_prompt = self.trompt_convs[i](x, x_prompt)
             # [batch_size, out_channels]
             out = self.trompt_decoder(x_prompt)
             # [batch_size, 1, out_channels]
