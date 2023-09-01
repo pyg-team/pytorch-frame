4526a4037625d3181a1665fdd26298936c3309cc
Yiwen Yuan <yiwen@kumo.ai>
2023-09-01 23:32:16 +0000

    test excelformer on classification problem

--- test/nn/models/test_excelformer.py
+++ test/nn/models/test_excelformer.py
@@ -1,15 +1,43 @@
 from torch_frame.data.dataset import Dataset
-from torch_frame.datasets.fake import FakeNumericalDataset
+from torch_frame.datasets.fake import (
+    FakeClassificationDatasetWithNumericalColsOnly,
+    FakeRegressionDatasetWithNumericalColsOnly,
+)
 from torch_frame.nn import ExcelFormer
 
 
-def test_excelformer():
+def test_excelformer_regression():
     batch_size = 10
     in_channels = 8
     out_channels = 1
     num_heads = 2
     num_layers = 6
-    dataset: Dataset = FakeNumericalDataset(num_rows=10, with_nan=False)
+    dataset: Dataset = FakeRegressionDatasetWithNumericalColsOnly(
+        num_rows=10, with_nan=False)
+    dataset.materialize()
+    num_cols = len(dataset.col_stats) - 1
+    tensor_frame = dataset.tensor_frame
+    model = ExcelFormer(
+        in_channels=in_channels,
+        out_channels=out_channels,
+        num_cols=num_cols,
+        num_layers=num_layers,
+        num_heads=num_heads,
+        col_stats=dataset.col_stats,
+        col_names_dict=tensor_frame.col_names_dict,
+    )
+    out = model(tensor_frame)
+    assert out.shape == (batch_size, num_cols, in_channels)
+
+
+def test_excelformer_classification():
+    batch_size = 10
+    in_channels = 8
+    out_channels = 1
+    num_heads = 2
+    num_layers = 6
+    dataset: Dataset = FakeClassificationDatasetWithNumericalColsOnly(
+        num_rows=10, with_nan=False)
     dataset.materialize()
     num_cols = len(dataset.col_stats) - 1
     tensor_frame = dataset.tensor_frame
--- torch_frame/datasets/fake.py
+++ torch_frame/datasets/fake.py
@@ -40,8 +40,8 @@ class FakeDataset(torch_frame.data.Dataset):
         super().__init__(df, col_to_stype, target_col='target')
 
 
-class FakeNumericalDataset(torch_frame.data.Dataset):
-    r"""A fake regression dataset with only numerical types.
+class FakeRegressionDatasetWithNumericalColsOnly(torch_frame.data.Dataset):
+    r"""A fake regression dataset with only numerical features.
 
     Args:
         num_rows (int): Number of rows.
@@ -66,6 +66,37 @@ class FakeNumericalDataset(torch_frame.data.Dataset):
             'b': stype.numerical,
             'c': stype.numerical,
             'target': stype.numerical,
+        }
+
+        super().__init__(df, col_to_stype, target_col='target')
+
+
+class FakeClassificationDatasetWithNumericalColsOnly(torch_frame.data.Dataset):
+    r"""A fake classification dataset with only numerical features.
+
+    Args:
+        num_rows (int): Number of rows.
+        with_nan (bool): Whether include nan in the dataset.
+    """
+    def __init__(
+        self,
+        num_rows: int,
+        with_nan: bool,
+    ):
+        df = pd.DataFrame({
+            'a': np.random.randn(num_rows),
+            'b': np.random.randn(num_rows),
+            'c': np.random.randn(num_rows),
+            'target': np.random.randint(0, 3, size=(num_rows, )),
+        })
+        if with_nan:
+            df.iloc[0] = df.iloc[-1] = np.nan
+
+        col_to_stype = {
+            'a': stype.numerical,
+            'b': stype.numerical,
+            'c': stype.numerical,
+            'target': stype.categorical,
         }
 
         super().__init__(df, col_to_stype, target_col='target')
