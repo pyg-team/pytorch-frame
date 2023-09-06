16f72897d3efbf729bb507b2ec05644d67429442
Yiwen Yuan <yiwen@kumo.ai>
2023-09-06 06:15:24 +0000

    initial test case implementation

--- test/transforms/test_categorical_catboost_encoder.py
+++ test/transforms/test_categorical_catboost_encoder.py
@@ -8,8 +8,10 @@ def test_categorical_catboost_encoder():
     dataset: Dataset = FakeDataset(num_rows=10, with_nan=False,
                                    stypes=[stype.categorical])
     dataset.materialize()
+    total_cols = len(dataset.feat_cols)
     tensor_frame: TensorFrame = dataset.tensor_frame
-    train_dataset = dataset.get_split_dataset('traim')
+    train_dataset = dataset.get_split_dataset('train')
     transform = CategoricalCatboostEncoder(train_dataset.tensor_frame)
     out = transform(tensor_frame)
     assert (len(out.col_names_dict[stype.categorical]) == 0)
+    assert (len(out.col_names_dict[stype.numerical]) == total_cols)
