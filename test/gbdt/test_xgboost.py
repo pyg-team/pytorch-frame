ddfa71ada106360372928681986d7965dd74baeb
Yiwen Yuan <yiwen@kumo.ai>
2023-09-10 21:28:43 +0000

    add regresssion test case

--- test/gbdt/test_xgboost.py
+++ test/gbdt/test_xgboost.py
@@ -4,7 +4,7 @@ from torch_frame.datasets.fake import FakeDataset
 from torch_frame.gbdt import XGBoost
 
 
-def test_xgboost():
+def test_xgboost_classification():
     dataset: Dataset = FakeDataset(num_rows=10, with_nan=False,
                                    stypes=[stype.numerical, stype.categorical],
                                    create_split=True,
@@ -13,9 +13,26 @@ def test_xgboost():
     train_dataset = dataset.get_split_dataset('train')
     val_dataset = dataset.get_split_dataset('val')
     test_dataset = dataset.get_split_dataset('test')
-    XGB = XGBoost(task_type=TaskType.MULTICLASS_CLASSIFICATION)
+    XGB = XGBoost(task_type=TaskType.BINARY_CLASSIFICATION)
     XGB.fit_tune(tf_train=train_dataset.tensor_frame,
                  tf_val=val_dataset.tensor_frame, num_trials=2,
                  num_boost_round=2)
     test_acc = XGB.eval(tf_test=test_dataset.tensor_frame)
     assert (test_acc >= 0 and test_acc <= 1)
+
+
+def test_xgboost_regression():
+    dataset: Dataset = FakeDataset(num_rows=10, with_nan=False,
+                                   stypes=[stype.numerical, stype.categorical],
+                                   create_split=True,
+                                   task_type=TaskType.REGRESSION)
+    dataset.materialize()
+    train_dataset = dataset.get_split_dataset('train')
+    val_dataset = dataset.get_split_dataset('val')
+    test_dataset = dataset.get_split_dataset('test')
+    XGB = XGBoost(task_type=TaskType.REGRESSION)
+    XGB.fit_tune(tf_train=train_dataset.tensor_frame,
+                 tf_val=val_dataset.tensor_frame, num_trials=2,
+                 num_boost_round=2)
+    mse = XGB.eval(tf_test=test_dataset.tensor_frame)
+    assert (mse >= 0)
