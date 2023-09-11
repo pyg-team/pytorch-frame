24ab1c34f8f48a3a216788565003ab0d791624b7
Yiwen Yuan <yiwen@kumo.ai>
2023-09-11 17:56:30 +0000

    fix

--- torch_frame/transforms/base_transform.py
+++ torch_frame/transforms/base_transform.py
@@ -1,5 +1,5 @@
 import copy
-from abc import ABC
+from abc import ABC, abstractmethod
 
 from torch_frame import TensorFrame
 
@@ -13,6 +13,7 @@ class BaseTransform(ABC):
         # Shallow-copy the data so that we prevent in-place data modification.
         return self.forward(copy.copy(tf))
 
+    @abstractmethod
     def forward(self, tf: TensorFrame) -> TensorFrame:
         r"""Process TensorFrame obj into another TensorFrame obj.
         Args:
@@ -21,6 +22,3 @@ class BaseTransform(ABC):
             tf (TensorFrame): Input :obj:`TensorFrame` after transform.
         """
         return tf
-
-    def __repr__(self) -> str:
-        return f'{self.__class__.__name__}()'
