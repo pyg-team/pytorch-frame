a00e9fda53d3dfecc6eb511e52899bea33b53716
Yiwen Yuan <yiwen@kumo.ai>
2023-09-13 17:30:34 +0000

    add missing values imputer

--- torch_frame/transforms/__init__.py
+++ torch_frame/transforms/__init__.py
@@ -2,10 +2,12 @@ from .base_transform import BaseTransform
 from .fittable_base_transform import FittableBaseTransform
 from .categorical_catboost_encoder import CategoricalCatBoostEncoder
 from .mutual_information_sort import MutualInformationSort
+from .missing_values_imputer import MissingValuesImputer
 
 __all__ = functions = [
     'BaseTransform',
     'CategoricalCatBoostEncoder',
     'FittableBaseTransform',
     'MutualInformationSort',
+    'MissingValuesImputer',
 ]
