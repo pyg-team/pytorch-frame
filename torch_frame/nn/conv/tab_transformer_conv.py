32c12b9c893911022acb5c2e442f52b35d4af135
Yiwen Yuan <yiwen@kumo.ai>
2023-09-18 01:58:28 +0000

    wip

---
+++ torch_frame/nn/conv/tab_transformer_conv.py
@@ -0,0 +1,14 @@
+import math
+
+import torch
+import torch.nn.functional as F
+from torch import Tensor
+from torch.nn import Dropout, LayerNorm, Linear, Module
+from torch.nn.init import zeros_
+
+from torch_frame.nn.conv import TableConv
+
+from ..utils.init import attenuated_kaiming_uniform_
+class TabTransformerConv(TableConv):
+    def __init__(self):
+        pass
--- torch_frame/nn/encoder/stype_encoder.py
+++ torch_frame/nn/encoder/stype_encoder.py
@@ -148,6 +148,12 @@ class StypeEncoder(Module, ABC):
             column_data[nan_mask] = fill_value
         return x
 
+class ColumnEncoder(StypeEncoder):
+    supported_stypes = {stype.categorical}
+
+    def __init__(self):
+        pass
+
 
 class EmbeddingEncoder(StypeEncoder):
     r"""An embedding look-up based encoder for categorical features. It
