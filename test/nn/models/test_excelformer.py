1816d208d2ba58114597db4228e927c0d2c149ae
Yiwen Yuan <yiwen@kumo.ai>
2023-09-21 22:48:05 +0000

    expand test

--- test/nn/models/test_excelformer.py
+++ test/nn/models/test_excelformer.py
@@ -1,18 +1,29 @@
+import pytest
+
+from torch_frame import TaskType
 from torch_frame.data.dataset import Dataset
 from torch_frame.datasets.fake import FakeDataset
 from torch_frame.nn import ExcelFormer
 from torch_frame.stype import stype
 
 
-def test_excelformer():
+@pytest.mark.parametrize('task_type', [
+    TaskType.REGRESSION, TaskType.BINARY_CLASSIFICATION,
+    TaskType.MULTICLASS_CLASSIFICATION
+])
+def test_excelformer(task_type):
     batch_size = 10
     in_channels = 8
-    out_channels = 1
     num_heads = 2
     num_layers = 6
     dataset: Dataset = FakeDataset(num_rows=10, with_nan=False,
-                                   stypes=[stype.numerical])
+                                   stypes=[stype.numerical],
+                                   task_type=task_type)
     dataset.materialize()
+    if dataset.task_type.is_classification:
+        out_channels = dataset.num_classes
+    else:
+        out_channels = 1
     num_cols = len(dataset.col_stats) - 1
     tensor_frame = dataset.tensor_frame
     model = ExcelFormer(in_channels=in_channels, out_channels=out_channels,
@@ -21,4 +32,8 @@ def test_excelformer():
                         col_names_dict=tensor_frame.col_names_dict)
     out_mixedup, y_mixedup = model(tensor_frame, mixup=True)
     assert out_mixedup.shape == (batch_size, out_channels)
-    assert y_mixedup.shape == tensor_frame.y.shape
+
+    if dataset.task_type.is_classification:
+        assert y_mixedup.shape == (batch_size, out_channels)
+    else:
+        assert y_mixedup.shape == tensor_frame.y.shape
