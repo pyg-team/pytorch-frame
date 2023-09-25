8e270bc34299aaf32c3b7c45592aac3b0b2e60bb
Weihua Hu <weihua916@gmail.com>
2023-09-25 11:41:26 -0700

    update

--- test/nn/models/test_tabnet.py
+++ test/nn/models/test_tabnet.py
@@ -7,10 +7,11 @@ def test_tabnet():
     dataset: Dataset = FakeDataset(num_rows=10, with_nan=False)
     dataset.materialize()
     tensor_frame = dataset.tensor_frame
-    tabnet = TabNet(out_channels=8, num_layers=3, split_feature_channels=8,
-                    split_attention_channels=8, gamma=1.2,
-                    col_stats=dataset.col_stats,
+    out_channels = 12
+    tabnet = TabNet(out_channels=out_channels, num_layers=3,
+                    split_feature_channels=8, split_attention_channels=8,
+                    gamma=1.2, col_stats=dataset.col_stats,
                     col_names_dict=tensor_frame.col_names_dict)
     out, reg = tabnet(tensor_frame, return_reg=True)
-    assert out.shape == (10, 8)
+    assert out.shape == (len(tensor_frame), out_channels)
     assert reg > 0
