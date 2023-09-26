04a151b25397969472ac358831e0bec5dfefe7e6
Weihua Hu <weihua916@gmail.com>
2023-09-25 21:28:50 -0700

    update

--- torch_frame/config/text_embedder.py
+++ torch_frame/config/text_embedder.py
@@ -6,5 +6,11 @@ from torch import Tensor
 
 @dataclass
 class TextEmbedderConfig:
+    # Text embedder model that maps a list of strings/sentences into Pytorch
+    # Tensor embeddings.
     text_embedder: Callable[[List[str]], Tensor]
+    # Batch size to use when encoding the sentences. It is recommended to set
+    # it to a reasonable value when one uses a heavy text embedding model
+    # (e.g., Transformer) on GPU. If set to :obj:`None`, the text embeddings
+    # will be obtained in a full-batch manner.
     batch_size: Optional[int]
