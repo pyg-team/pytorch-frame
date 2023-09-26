e73b24e5f9081c09a6985b3f8d185b40bfa03008
Weihua Hu <weihua916@gmail.com>
2023-09-25 21:19:11 -0700

    update

--- torch_frame/testing/text_embedder.py
+++ torch_frame/testing/text_embedder.py
@@ -6,7 +6,7 @@ from torch.nn import Embedding
 
 
 class HashTextEmbedder:
-    r"""A fake light-weight has-based text embedder for testing purpose.
+    r"""A hash-based light-weight text embedder for testing purposes.
     It hashes each sentence into an index modulo :obj:`num_hash` and then
     uses :class:`torch.nn.Embedding` to look up the index to obtain the
     sentence embedding.
@@ -16,14 +16,15 @@ class HashTextEmbedder:
         num_hash (int): Number of hashes to use.
         device (torch.device, optional): The device to put Embedding module.
     """
-    def __init__(self, out_channels: int, num_hash: int = 64,
+    def __init__(self, out_channels: int, num_hash_bins: int = 64,
                  device: Optional[torch.device] = None):
         self.out_channels = out_channels
-        self.num_hash = num_hash
+        self.num_hash_bins = num_hash_bins
         self.device = device
-        self.embedding = Embedding(num_hash, out_channels).to(device)
+        self.embedding = Embedding(num_hash_bins, out_channels).to(device)
 
     def __call__(self, sentences: List[str]) -> Tensor:
-        idx = torch.LongTensor([hash(s) % self.num_hash for s in sentences],
-                               device=self.device)
+        idx = torch.LongTensor(
+            [hash(s) % self.num_hash_bins for s in sentences],
+            device=self.device)
         return self.embedding(idx)
