641afec7d241a014a3c3920783b1ad7a157eadd8
Weihua Hu <weihua916@gmail.com>
2023-09-28 23:41:29 +0000

    remove

--- examples/trompt.sh
+++
@@ -1,7 +0,0 @@
-for dataset in bank-marketing compas-two-years MagicTelescope
-do
-echo $dataset
-python trompt.py --dataset $dataset
-done
-
-# electricity eye_movements california credit jannis pol 
