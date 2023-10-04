6a635baf1687dd8714d243a697c37c1efc914f57
Zecheng Zhang <zecheng@kumo.ai>
2023-10-04 11:10:56 -0700

    Add text embedded example

---
+++ examples/fttransformer_with_bert.py
@@ -0,0 +1,144 @@
+import argparse
+import os.path as osp
+from typing import List
+
+import torch
+import torch.nn.functional as F
+from torch import Tensor
+from tqdm import tqdm
+from transformers import AutoTokenizer, BertModel
+
+from torch_frame import stype
+from torch_frame.config.text_embedder import TextEmbedderConfig
+from torch_frame.data import DataLoader
+from torch_frame.datasets import MultimodalTextBenchmark
+from torch_frame.nn import (
+    EmbeddingEncoder,
+    FTTransformer,
+    LinearEmbeddingEncoder,
+    LinearEncoder,
+)
+
+
+class PretrainedBERTEncoder:
+    def __init__(self):
+        self.tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+        self.model = BertModel.from_pretrained("bert-base-uncased")
+
+    def __call__(self, sentences: List[str]) -> Tensor:
+        embeddings = []
+        for i, sentence in enumerate(sentences):
+            tokens = self.tokenizer(sentence, return_tensors="pt")
+            # CLS embedding
+            embedding = self.model(**tokens).last_hidden_state[:, 0].detach()
+            embeddings.append(embedding)
+            if i % 1000 == 0:
+                print(f"Encoded {i} sentences")
+        return torch.concat(embeddings, dim=0)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', type=str, default='wine_reviews')
+parser.add_argument('--channels', type=int, default=256)
+parser.add_argument('--num_layers', type=int, default=4)
+parser.add_argument('--batch_size', type=int, default=512)
+parser.add_argument('--lr', type=float, default=0.0001)
+parser.add_argument('--epochs', type=int, default=100)
+parser.add_argument('--seed', type=int, default=0)
+args = parser.parse_args()
+
+torch.manual_seed(args.seed)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Prepare datasets
+path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data',
+                args.dataset)
+bert_encoder = PretrainedBERTEncoder()
+dataset = MultimodalTextBenchmark(
+    root=path,
+    name=args.dataset,
+    text_embedder_cfg=TextEmbedderConfig(text_embedder=bert_encoder,
+                                         batch_size=None),
+)
+
+dataset.materialize()
+
+train_dataset = dataset.get_split_dataset('train')
+val_dataset = dataset.get_split_dataset('val')
+test_dataset = dataset.get_split_dataset('test')
+
+# Set up data loaders
+train_tensor_frame = train_dataset.tensor_frame.to(device)
+val_tensor_frame = val_dataset.tensor_frame.to(device)
+test_tensor_frame = test_dataset.tensor_frame.to(device)
+train_loader = DataLoader(train_tensor_frame, batch_size=args.batch_size,
+                          shuffle=True)
+val_loader = DataLoader(val_tensor_frame, batch_size=args.batch_size)
+test_loader = DataLoader(test_tensor_frame, batch_size=args.batch_size)
+
+stype_encoder_dict = {
+    stype.categorical: EmbeddingEncoder(),
+    stype.numerical: LinearEncoder(),
+    stype.text_embedded: LinearEmbeddingEncoder(in_channels=768)
+}
+
+model = FTTransformer(
+    channels=args.channels,
+    out_channels=dataset.num_classes,
+    num_layers=args.num_layers,
+    col_stats=dataset.col_stats,
+    col_names_dict=train_tensor_frame.col_names_dict,
+    stype_encoder_dict=stype_encoder_dict,
+).to(device)
+
+optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr)
+
+
+def train(epoch: int) -> float:
+    model.train()
+    loss_accum = total_count = 0
+
+    for tf in tqdm(train_loader, desc=f'Epoch: {epoch}'):
+        pred = model(tf)
+        loss = F.cross_entropy(pred, tf.y)
+        optimizer.zero_grad()
+        loss.backward()
+        loss_accum += float(loss) * len(tf.y)
+        total_count += len(tf.y)
+        optimizer.step()
+    return loss_accum / total_count
+
+
+@torch.no_grad()
+def test(loader: DataLoader) -> float:
+    model.eval()
+    accum = total_count = 0
+
+    for tf in loader:
+        pred = model(tf)
+        pred_class = pred.argmax(dim=-1)
+        accum += float((tf.y == pred_class).sum())
+        total_count += len(tf.y)
+
+    accuracy = accum / total_count
+    return accuracy
+
+
+metric = 'Acc'
+best_val_metric = 0
+best_test_metric = 0
+
+for epoch in range(1, args.epochs + 1):
+    train_loss = train(epoch)
+    train_metric = test(train_loader)
+    val_metric = test(val_loader)
+    test_metric = test(test_loader)
+
+    best_val_metric = val_metric
+    best_test_metric = test_metric
+
+    print(f'Train Loss: {train_loss:.4f}, Train {metric}: {train_metric:.4f}, '
+          f'Val {metric}: {val_metric:.4f}, Test {metric}: {test_metric:.4f}')
+
+print(f'Best Val {metric}: {best_val_metric:.4f}, '
+      f'Best Test {metric}: {best_test_metric:.4f}')
