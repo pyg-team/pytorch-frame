be1c7aa047a2e25c94af4a8f9281a7a2aa5d2b39
Weihua Hu <weihua916@gmail.com>
2023-10-11 20:33:23 -0700

    update

--- torch_frame/utils/split.py
+++ torch_frame/utils/split.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+# Mapping split name to integer.
 SPLIT_TO_NUM = {'train': 0, 'val': 1, 'test': 2}
 
 
@@ -7,9 +8,9 @@ def generate_random_split(length: int, seed: int, train_ratio: float = 0.8,
                           val_ratio: float = 0.1) -> np.ndarray:
     r"""Generate a list of random split assignments of the specified length.
     The elements are either :obj:`0`, :obj:`1`, or :obj:`2`, representing
-    train, val, test, respectively. Note that this relies on the fact that
-    numpy's shuffle is consistent across versions, which has been historically
-    the case."""
+    train, val, test, respectively. Note that this function relies on the fact
+    that numpy's shuffle is consistent across versions, which has been
+    historically the case."""
     assert train_ratio + val_ratio < 1
     assert train_ratio > 0
     assert train_ratio > 0
