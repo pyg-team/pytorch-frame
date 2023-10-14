319b8ae587aa618c3a82e44ee854e8ca13880db7
Weihua Hu <weihua916@gmail.com>
2023-10-14 01:45:05 +0000

    update

--- torch_frame/datasets/data_frame_benchmark.py
+++ torch_frame/datasets/data_frame_benchmark.py
@@ -714,12 +714,19 @@ class DataFrameBenchmark(torch_frame.data.Dataset):
     }
 
     @classmethod
-    def datasets_available(cls, task_type: TaskType,
-                           scale: str) -> List[Tuple[str, Dict[str, Any]]]:
+    def datasets_available(
+        cls,
+        task_type: TaskType,
+        scale: str,
+    ) -> List[Tuple[str, Dict[str, Any]]]:
+        r"""List of datasets available for a given :obj:`task_type` and
+        :obj:`scale`."""
         return cls.dataset_categorization_dict[task_type.value][scale]
 
     @classmethod
     def num_datasets_available(cls, task_type: TaskType, scale: str):
+        r"""Number of datasets available for a given :obj:`task_type` and
+        :obj:`scale`."""
         return len(cls.datasets_available(task_type, scale))
 
     def __init__(
