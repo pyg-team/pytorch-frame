3415f7b07746e692359333d2a2cbbda23252bdcd
Weihua Hu <weihua916@gmail.com>
2023-10-14 02:46:07 +0000

    add gbdt

--- examples/data_frame_benchmark.py
+++ examples/data_frame_benchmark.py
@@ -16,6 +16,7 @@ from tqdm import tqdm
 from torch_frame import stype
 from torch_frame.data import DataLoader
 from torch_frame.datasets import DataFrameBenchmark
+from torch_frame.gbdt import CatBoost, XGBoost
 from torch_frame.nn.encoder import EmbeddingEncoder, LinearBucketEncoder
 from torch_frame.nn.models import (
     ExcelFormer,
@@ -28,6 +29,7 @@ from torch_frame.nn.models import (
 from torch_frame.typing import TaskType
 
 TRAIN_CONFIG_KEYS = ["batch_size", "gamma_rate", "base_lr"]
+GBDT_MODELS = ["XGBoost", "CatBoost"]
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -48,13 +50,8 @@ parser.add_argument(
     help='Number of repeated training and eval on the best config.')
 parser.add_argument(
     '--model_type', type=str, default='TabNet', choices=[
-        'TabNet',
-        'FTTransformer',
-        'ResNet',
-        'TabTransformer',
-        'Trompt',
-        'ExcelFormer',
-        'FTTransformerBucket',
+        'TabNet', 'FTTransformer', 'ResNet', 'TabTransformer', 'Trompt',
+        'ExcelFormer', 'FTTransformerBucket', 'XGBoost', 'CatBoost'
     ])
 parser.add_argument('--seed', type=int, default=0)
 parser.add_argument('--result_path', type=str, default='')
@@ -77,146 +74,152 @@ train_tensor_frame = train_dataset.tensor_frame
 val_tensor_frame = val_dataset.tensor_frame
 test_tensor_frame = test_dataset.tensor_frame
 
-if dataset.task_type == TaskType.BINARY_CLASSIFICATION:
-    out_channels = 1
-    loss_fun = BCEWithLogitsLoss()
-    metric_computer = AUROC(task='binary').to(device)
-    higher_is_better = True
-elif dataset.task_type == TaskType.MULTICLASS_CLASSIFICATION:
-    out_channels = dataset.num_classes
-    loss_fun = CrossEntropyLoss()
-    metric_computer = Accuracy(task='multiclass',
-                               num_classes=dataset.num_classes).to(device)
-    higher_is_better = True
-elif dataset.task_type == TaskType.REGRESSION:
-    out_channels = 1
-    loss_fun = MSELoss()
-    metric_computer = MeanSquaredError(squared=False).to(device)
-    higher_is_better = False
+if args.model_type in GBDT_MODELS:
+    gbdt_cls_dict = {'XGBoost': XGBoost, 'CatBoost': CatBoost}
+    model_cls = gbdt_cls_dict[args.model_type]
+else:
+    if dataset.task_type == TaskType.BINARY_CLASSIFICATION:
+        out_channels = 1
+        loss_fun = BCEWithLogitsLoss()
+        metric_computer = AUROC(task='binary').to(device)
+        higher_is_better = True
+    elif dataset.task_type == TaskType.MULTICLASS_CLASSIFICATION:
+        out_channels = dataset.num_classes
+        loss_fun = CrossEntropyLoss()
+        metric_computer = Accuracy(task='multiclass',
+                                   num_classes=dataset.num_classes).to(device)
+        higher_is_better = True
+    elif dataset.task_type == TaskType.REGRESSION:
+        out_channels = 1
+        loss_fun = MSELoss()
+        metric_computer = MeanSquaredError(squared=False).to(device)
+        higher_is_better = False
 
-# To be set for each model
-model_cls = None
-col_stats = None
+    # To be set for each model
+    model_cls = None
+    col_stats = None
 
-# Set up model specific search space
-if args.model_type == 'TabNet':
-    model_search_space = {
-        'split_attention_channels': [64, 128, 256],
-        'split_feature_channels': [64, 128, 256],
-        'gamma': [1., 1.2, 1.5],
-        'num_layers': [4, 6, 8],
-    }
-    train_search_space = {
-        'batch_size': [2048, 4096],
-        'base_lr': [0.001, 0.01],
-        'gamma_rate': [0.9, 0.95, 1.],
-    }
-    model_cls = TabNet
-    col_stats = dataset.col_stats
-elif args.model_type == 'FTTransformer':
-    model_search_space = {
-        'channels': [64, 128, 256],
-        'num_layers': [4, 6, 8],
-    }
-    train_search_space = {
-        'batch_size': [256, 512],
-        'base_lr': [0.0001, 0.001],
-        'gamma_rate': [0.9, 0.95, 1.],
-    }
-    model_cls = FTTransformer
-    col_stats = dataset.col_stats
-elif args.model_type == 'FTTransformerBucket':
-    model_search_space = {
-        'channels': [64, 128, 256],
-        'num_layers': [4, 6, 8],
-    }
-    train_search_space = {
-        'batch_size': [256, 512],
-        'base_lr': [0.0001, 0.001],
-        'gamma_rate': [0.9, 0.95, 1.],
-    }
-    model_cls = FTTransformer
+    # Set up model specific search space
+    if args.model_type == 'TabNet':
+        model_search_space = {
+            'split_attention_channels': [64, 128, 256],
+            'split_feature_channels': [64, 128, 256],
+            'gamma': [1., 1.2, 1.5],
+            'num_layers': [4, 6, 8],
+        }
+        train_search_space = {
+            'batch_size': [2048, 4096],
+            'base_lr': [0.001, 0.01],
+            'gamma_rate': [0.9, 0.95, 1.],
+        }
+        model_cls = TabNet
+        col_stats = dataset.col_stats
+    elif args.model_type == 'FTTransformer':
+        model_search_space = {
+            'channels': [64, 128, 256],
+            'num_layers': [4, 6, 8],
+        }
+        train_search_space = {
+            'batch_size': [256, 512],
+            'base_lr': [0.0001, 0.001],
+            'gamma_rate': [0.9, 0.95, 1.],
+        }
+        model_cls = FTTransformer
+        col_stats = dataset.col_stats
+    elif args.model_type == 'FTTransformerBucket':
+        model_search_space = {
+            'channels': [64, 128, 256],
+            'num_layers': [4, 6, 8],
+        }
+        train_search_space = {
+            'batch_size': [256, 512],
+            'base_lr': [0.0001, 0.001],
+            'gamma_rate': [0.9, 0.95, 1.],
+        }
+        model_cls = FTTransformer
 
-    col_stats = dataset.col_stats
-elif args.model_type == 'ResNet':
-    model_search_space = {
-        'channels': [64, 128, 256],
-        'num_layers': [4, 6, 8],
-    }
-    train_search_space = {
-        'batch_size': [256, 512],
-        'base_lr': [0.0001, 0.001],
-        'gamma_rate': [0.9, 0.95, 1.],
-    }
-    model_cls = ResNet
-    col_stats = dataset.col_stats
-elif args.model_type == 'TabTransformer':
-    model_search_space = {
-        'channels': [32, 64, 128],
-        'num_layers': [4, 6, 8],
-        'num_heads': [2, 4, 8],
-        'encoder_pad_size': [2],
-        'attn_dropout': [0, 0.2],
-        'ffn_dropout': [0, 0.2],
-    }
-    train_search_space = {
-        'batch_size': [128, 256],
-        'base_lr': [0.0001, 0.001],
-        'gamma_rate': [0.9, 0.95, 1.],
-    }
-    model_cls = TabTransformer
-    col_stats = dataset.col_stats
-elif args.model_type == 'Trompt':
-    model_search_space = {
-        'channels': [128],
-        'num_layers': [4, 6, 8],
-        'num_prompts': [128],
-    }
-    train_search_space = {
-        'batch_size': [128, 256],
-        'base_lr': [0.01, 0.001],
-        'gamma_rate': [0.9, 0.95, 1.],
-    }
-    model_cls = Trompt
-    col_stats = dataset.col_stats
-elif args.model_type == 'ExcelFormer':
-    from torch_frame.transforms import CatToNumTransform, MutualInformationSort
+        col_stats = dataset.col_stats
+    elif args.model_type == 'ResNet':
+        model_search_space = {
+            'channels': [64, 128, 256],
+            'num_layers': [4, 6, 8],
+        }
+        train_search_space = {
+            'batch_size': [256, 512],
+            'base_lr': [0.0001, 0.001],
+            'gamma_rate': [0.9, 0.95, 1.],
+        }
+        model_cls = ResNet
+        col_stats = dataset.col_stats
+    elif args.model_type == 'TabTransformer':
+        model_search_space = {
+            'channels': [32, 64, 128],
+            'num_layers': [4, 6, 8],
+            'num_heads': [2, 4, 8],
+            'encoder_pad_size': [2],
+            'attn_dropout': [0, 0.2],
+            'ffn_dropout': [0, 0.2],
+        }
+        train_search_space = {
+            'batch_size': [128, 256],
+            'base_lr': [0.0001, 0.001],
+            'gamma_rate': [0.9, 0.95, 1.],
+        }
+        model_cls = TabTransformer
+        col_stats = dataset.col_stats
+    elif args.model_type == 'Trompt':
+        model_search_space = {
+            'channels': [128],
+            'num_layers': [4, 6, 8],
+            'num_prompts': [128],
+        }
+        train_search_space = {
+            'batch_size': [128, 256],
+            'base_lr': [0.01, 0.001],
+            'gamma_rate': [0.9, 0.95, 1.],
+        }
+        model_cls = Trompt
+        col_stats = dataset.col_stats
+    elif args.model_type == 'ExcelFormer':
+        from torch_frame.transforms import (
+            CatToNumTransform,
+            MutualInformationSort,
+        )
 
-    categorical_transform = CatToNumTransform()
-    categorical_transform.fit(train_dataset.tensor_frame,
-                              train_dataset.col_stats)
-    train_tensor_frame = categorical_transform(train_tensor_frame)
-    val_tensor_frame = categorical_transform(val_tensor_frame)
-    test_tensor_frame = categorical_transform(test_tensor_frame)
-    col_stats = categorical_transform.transformed_stats
+        categorical_transform = CatToNumTransform()
+        categorical_transform.fit(train_dataset.tensor_frame,
+                                  train_dataset.col_stats)
+        train_tensor_frame = categorical_transform(train_tensor_frame)
+        val_tensor_frame = categorical_transform(val_tensor_frame)
+        test_tensor_frame = categorical_transform(test_tensor_frame)
+        col_stats = categorical_transform.transformed_stats
 
-    mutual_info_sort = MutualInformationSort(task_type=dataset.task_type)
-    mutual_info_sort.fit(train_tensor_frame, col_stats)
-    train_tensor_frame = mutual_info_sort(train_tensor_frame)
-    val_tensor_frame = mutual_info_sort(val_tensor_frame)
-    test_tensor_frame = mutual_info_sort(test_tensor_frame)
+        mutual_info_sort = MutualInformationSort(task_type=dataset.task_type)
+        mutual_info_sort.fit(train_tensor_frame, col_stats)
+        train_tensor_frame = mutual_info_sort(train_tensor_frame)
+        val_tensor_frame = mutual_info_sort(val_tensor_frame)
+        test_tensor_frame = mutual_info_sort(test_tensor_frame)
 
-    model_search_space = {
-        'in_channels': [128, 256],
-        'num_heads': [8, 16, 32],
-        'num_layers': [4, 6, 8],
-        'diam_dropout': [0, 0.2],
-        'residual_dropout': [0, 0.2],
-        'aium_dropout': [0, 0.2],
-        'num_cols': [train_tensor_frame.num_cols],
-    }
-    train_search_space = {
-        'batch_size': [256, 512],
-        'base_lr': [0.001],
-        'gamma_rate': [0.9, 0.95, 1.],
-    }
-    model_cls = ExcelFormer
+        model_search_space = {
+            'in_channels': [128, 256],
+            'num_heads': [8, 16, 32],
+            'num_layers': [4, 6, 8],
+            'diam_dropout': [0, 0.2],
+            'residual_dropout': [0, 0.2],
+            'aium_dropout': [0, 0.2],
+            'num_cols': [train_tensor_frame.num_cols],
+        }
+        train_search_space = {
+            'batch_size': [256, 512],
+            'base_lr': [0.001],
+            'gamma_rate': [0.9, 0.95, 1.],
+        }
+        model_cls = ExcelFormer
 
-assert model_cls is not None
-assert col_stats is not None
-assert set(train_search_space.keys()) == set(TRAIN_CONFIG_KEYS)
-
-col_names_dict = train_tensor_frame.col_names_dict
+    assert model_cls is not None
+    assert col_stats is not None
+    assert set(train_search_space.keys()) == set(TRAIN_CONFIG_KEYS)
+    col_names_dict = train_tensor_frame.col_names_dict
 
 
 def train(
@@ -349,7 +352,7 @@ def objective(trial: optuna.trial.Trial) -> float:
     return best_val_metric
 
 
-def main():
+def main_deep_models():
     # Hyper-parameter optimization with Optuna
     print("Hyper-parameter search via Optuna")
     start_time = time.time()
@@ -379,21 +382,61 @@ def main():
         best_test_metrics.append(best_test_metric)
     end_time = time.time()
     final_model_time = (end_time - start_time) / args.num_repeats
+    best_val_metrics = np.array(best_val_metrics)
+    best_test_metrics = np.array(best_test_metrics)
 
     # Save results
     if args.result_path != '':
         result_dict = {
             'args': args.__dict__,
-            'best_val_metrics': np.array(best_val_metrics),
-            'best_test_metrics': np.array(best_val_metrics),
+            'best_val_metrics': best_val_metrics,
+            'best_test_metrics': best_test_metrics,
+            'best_val_metric': best_val_metrics.mean(),
+            'best_test_metric': best_test_metrics.mean(),
             'best_train_cfg': best_train_cfg,
             'best_model_cfg': best_model_cfg,
             'search_time': search_time,
             'final_model_time': final_model_time,
+            'total_time': search_time + final_model_time,
+        }
+        os.makedirs(os.path.dirname(args.result_path), exist_ok=True)
+        torch.save(result_dict, args.result_path)
+
+
+def main_gbdt():
+    if dataset.task_type.is_classification:
+        num_classes = dataset.num_classes
+    else:
+        num_classes = None
+    model = model_cls(task_type=dataset.task_type, num_classes=num_classes)
+
+    import time
+    start_time = time.time()
+    model.tune(tf_train=train_dataset.tensor_frame,
+               tf_val=val_dataset.tensor_frame, num_trials=args.num_trials)
+    val_pred = model.predict(tf_test=val_dataset.tensor_frame)
+    val_metric = model.compute_metric(val_dataset.tensor_frame.y,
+                                      val_pred)[model.metric]
+    test_pred = model.predict(tf_test=test_dataset.tensor_frame)
+    test_metric = model.compute_metric(test_dataset.tensor_frame.y,
+                                       test_pred)[model.metric]
+    end_time = time.time()
+
+    # Save results
+    if args.result_path != '':
+        result_dict = {
+            'args': args.__dict__,
+            'best_val_metric': val_metric,
+            'best_test_metric': test_metric,
+            'best_cfg': model.params,
+            'total_time': end_time - start_time,
         }
         os.makedirs(os.path.dirname(args.result_path), exist_ok=True)
         torch.save(result_dict, args.result_path)
 
 
 if __name__ == '__main__':
-    main()
+    if args.model_type in ["XGBoost", "CatBoost"]:
+        main_gbdt()
+    else:
+        main_deep_models()
