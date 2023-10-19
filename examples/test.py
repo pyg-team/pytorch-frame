0a9f975e469a151fed1c649cc9759c36c3f5ca1c
yiweny <yyuanlisette@gmail.com>
2023-10-19 21:34:00 +0000

    remove unnecessary file

--- examples/test.py
+++
@@ -1,7 +0,0 @@
-import os.path as osp
-
-from torch_frame.datasets import Mercari
-
-path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data')
-dataset = Mercari(root=path)
-print(dataset.df)
