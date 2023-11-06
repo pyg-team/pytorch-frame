fd16cdb477ad147ee88cac21b6e681d36eebf590
Akihiro Nitta <nitta@akihironitta.com>
2023-11-06 05:05:25 +0000

    Add MultiEmbeddingTensor
    [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci

---
+++ test/data/test_multi_embedding_tensor.py
@@ -0,0 +1,84 @@
+from typing import List
+import random
+
+import pytest
+import torch
+
+from torch_frame.data.multi_embedding_tensor import MultiEmbeddingTensor
+
+
+def assert_equal(
+    tensor_list: List[torch.Tensor],
+    met: MultiEmbeddingTensor,
+) -> None:
+    assert len(tensor_list) == met.num_cols
+    assert len(tensor_list[0]) == met.num_rows
+    for i in range(met.num_rows):
+        for j in range(met.num_cols):
+            # compare tensor at (row_i, col_j)
+            assert torch.allclose(tensor_list[j][i], met[i, j])
+
+
+def test_size():
+    num_rows = 8
+    num_cols = 3
+    lengths = torch.tensor([1, 2, 3])
+    offset = torch.tensor([0, 1, 3, 6])
+    values = torch.rand((num_rows, lengths.sum().item()))
+    met = MultiEmbeddingTensor(
+        num_rows=num_rows,
+        num_cols=num_cols,
+        values=values,
+        offset=offset,
+    )
+
+    assert met.size(0) == num_rows
+    assert met.size(1) == num_cols
+    with pytest.raises(IndexError, match="not have a fixed length"):
+        met.size(2)
+    with pytest.raises(IndexError, match="Dimension out of range"):
+        met.size(3)
+
+    assert met.shape[0] == num_rows
+    assert met.shape[1] == num_cols
+    assert met.shape[2] == -1
+
+
+def test_from_list():
+    num_rows = 2
+    num_cols = 3
+    tensor_list = [
+        torch.tensor([[0, 1, 2], [3, 4, 5]]),
+        torch.tensor([[6, 7], [8, 9]]),
+        torch.tensor([[10], [11]]),
+    ]
+    met = MultiEmbeddingTensor.from_list(tensor_list)
+    assert met.num_rows == num_rows
+    assert met.num_cols == num_cols
+    expected_values = torch.tensor([
+        [0, 1, 2, 6, 7, 10],
+        [3, 4, 5, 8, 9, 11],
+    ])
+    assert torch.allclose(met.values, expected_values)
+    expected_offset = torch.tensor([0, 3, 5, 6])
+    assert torch.allclose(met.offset, expected_offset)
+    assert_equal(tensor_list, met)
+
+    # case: empty list
+    with pytest.raises(AssertionError):
+        MultiEmbeddingTensor.from_list([])
+
+    # case: list of non-2d tensors
+    with pytest.raises(AssertionError):
+        MultiEmbeddingTensor.from_list([torch.rand(1)])
+
+    # case: list of tensors having different num_rows
+    with pytest.raises(AssertionError):
+        MultiEmbeddingTensor.from_list([torch.rand(2, 1), torch.rand(3, 1)])
+
+    # case: list of tensors on different devices
+    with pytest.raises(AssertionError):
+        MultiEmbeddingTensor.from_list([
+            torch.rand(2, 1, device="cpu"),
+            torch.rand(2, 1, device="meta"),
+        ])
---
+++ torch_frame/data/multi_embedding_tensor.py
@@ -0,0 +1,115 @@
+from typing import Sequence, List, Union, Any
+
+import torch
+from torch import Tensor
+
+from torch_frame.data.multi_tensor import _MultiTensor
+
+
+class MultiEmbeddingTensor(_MultiTensor):
+    r"""A PyTorch tensor-based data structure that stores
+    :obj:`[num_rows, num_cols, *]`, where the size of last dimension can be
+    different for different column.
+
+    Note that the last dimension is the same within each column across rows while
+    in :class:`MultiNestedTensor`, the last dimension can be different across both
+    rows and columns.
+
+    Args:
+        num_rows (int): Number of rows.
+        num_cols (int): Number of columns.
+        values (torch.Tensor): The values :class:`torch.Tensor` of size
+            :obj:`[num_rows, dim1+dim2+...+dimN]`.
+        offset (torch.Tensor): The offset :class:`torch.Tensor` of size
+            :obj:`[num_cols+1,]`.
+
+    Example:
+        >>> num_rows = 2
+        >>> tensor_list = [
+        ...    torch.tensor([[0, 1, 2], [3, 4, 5]]),  # col0
+        ...    torch.tensor([[6, 7], [8, 9]]),        # col1
+        ...    torch.tensor([[10], [11]]),            # col2
+        ... ]
+        >>> out = MultiEmbeddingTensor.from_list(tensor_list)
+        >>> out
+        MultiEmbeddingTensor(num_rows=2, num_cols=3, device='cpu')
+        >>> out[0, 0]
+        tensor([0, 1, 2]
+    """
+    def __init__(
+        self,
+        num_rows: int,
+        num_cols: int,
+        values: Tensor,
+        offset: Tensor,
+    ) -> None:
+        super().__init__(num_rows, num_cols, values, offset)
+        assert offset[0] == 0
+        assert len(offset) == num_cols + 1
+
+    def __getitem__(
+        self,
+        index: Any,
+    ) -> Union['MultiEmbeddingTensor', Tensor]:
+        # TODO: normalize index
+        if isinstance(index, tuple) and len(index) == 2 and isinstance(index[0], int) and isinstance(index[1], int):
+            # return self.values[self.offset[index[1]]:self.offset[index[1] + 1]]
+            # self.values: [num_rows, dim1+dim2+...+dimN]
+            i = index[0]
+            j = index[1]
+            return self.values[i, self.offset[j]:self.offset[j + 1]]
+
+        # TODO: Support more index types.
+        raise NotImplementedError
+
+    @classmethod
+    def from_list(
+        cls,
+        tensor_list: List[Tensor],
+    ) -> 'MultiEmbeddingTensor':
+        r"""Creates a :class:`MultiEmbeddingTensor` from a list of :class:`torch.Tensor`.
+
+        Args:
+            tensor_list (List[Tensor]): A list of tensors, where each tensor
+                has the same number of rows and can have a different number of
+                columns.
+
+        Returns:
+            MultiEmbeddingTensor: A :class:`MultiEmbeddingTensor` instance.
+
+        Example:
+            >>> num_rows = 2
+            >>> tensor_list = [
+            ...    torch.tensor([[0, 1, 2], [3, 4, 5]]),  # col0
+            ...    torch.tensor([[6, 7], [8, 9]]),        # col1
+            ...    torch.tensor([[10], [11]]),            # col2
+            ... ]
+            >>> out = MultiEmbeddingTensor.from_list(tensor_list)
+            >>> out
+            MultiEmbeddingTensor(num_rows=2, num_cols=3, device='cpu')
+            >>> out[0, 0]
+            tensor([0, 1, 2])
+        """
+        assert isinstance(tensor_list, list) and len(tensor_list) > 0
+        num_rows = tensor_list[0].size(0)
+        device = tensor_list[0].device
+        for tensor in tensor_list:
+            assert isinstance(tensor, torch.Tensor), "tensor_list must be a list of tensors."
+            assert tensor.dim() == 2, "tensor_list must be a list of 2D tensors."
+            assert tensor.size(0) == num_rows, "num_rows must be the same across a list of input tensors."
+            assert tensor.device == device, "device must be the same across a list of input tensors."
+
+        offset_list = []
+        accum_idx = 0
+        offset_list.append(accum_idx)
+        for tensor in tensor_list:
+            accum_idx += tensor.size(1)
+            offset_list.append(accum_idx)
+
+        num_cols = len(tensor_list)
+        # input: [num_rows, dim1], [num_rows, dim2], ..., [num_rows, dimN]
+        # output: [num_rows, dim1+dim2+...+dimN]
+        values = torch.cat(tensor_list, dim=1)
+        assert values.size() == (num_rows, offset_list[-1])
+        offset = torch.LongTensor(offset_list)
+        return cls(num_rows, num_cols, values, offset)
