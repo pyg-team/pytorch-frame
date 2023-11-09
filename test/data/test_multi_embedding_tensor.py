48c9bd525346e4e59b236aeaab4fedcc2127698e
Akihiro Nitta <nitta@akihironitta.com>
2023-11-09 04:01:42 +0000

    Add MultiEmbeddingTensor.cat
    [ghstack-poisoned]

--- test/data/test_multi_embedding_tensor.py
+++ test/data/test_multi_embedding_tensor.py
@@ -1,5 +1,5 @@
 import random
-from typing import List, Tuple
+from typing import List, Optional, Tuple
 
 import pytest
 import torch
@@ -27,6 +27,7 @@ def get_fake_multi_embedding_tensor(
     tensor_list = []
     for _ in range(num_cols):
         embedding_dim = random.randint(1, 5)
+        print(embedding_dim)
         tensor = torch.randn((num_rows, embedding_dim))
         tensor_list.append(tensor)
     return MultiEmbeddingTensor.from_tensor_list(tensor_list), tensor_list
@@ -117,3 +118,73 @@ def test_clone():
     assert met_clone.values[0, 0] != 12345.
     met.offset[0] = -1
     assert met_clone.offset[0] != -1
+
+
+def test_cat():
+    # case: dim=0
+    tensor_list1 = [
+        torch.tensor([[0, 1, 2], [6, 7, 8]]),
+        torch.tensor([[3, 4], [9, 10]]),
+        torch.tensor([[5], [11]]),
+    ]
+    met1 = MultiEmbeddingTensor.from_tensor_list(tensor_list1)
+    tensor_list2 = [
+        torch.tensor([[12, 13, 14]]),
+        torch.tensor([[15, 16]]),
+        torch.tensor([[17]]),
+    ]
+    met2 = MultiEmbeddingTensor.from_tensor_list(tensor_list2)
+    for xs in [[met1, met2], (met1, met2)]:
+        met_cat = MultiEmbeddingTensor.cat(xs, dim=0)
+        assert met_cat.num_rows == met1.num_rows + met2.num_rows
+        assert met_cat.num_cols == met1.num_cols == met2.num_cols
+        expected_values = torch.tensor(
+            [
+                [0, 1, 2, 3, 4, 5],
+                [6, 7, 8, 9, 10, 11],
+                [12, 13, 14, 15, 16, 17],
+            ]
+        )
+        assert torch.allclose(met_cat.values, expected_values)
+        assert torch.allclose(met_cat.offset, met1.offset)
+        assert torch.allclose(met_cat.offset, met2.offset)
+
+        met_cat = MultiEmbeddingTensor.cat((met1, met2), dim=0)
+        assert met_cat.num_rows == met1.num_rows + met2.num_rows
+        assert met_cat.num_cols == met1.num_cols == met2.num_cols
+        expected_values = torch.tensor(
+            [
+                [0, 1, 2, 3, 4, 5],
+                [6, 7, 8, 9, 10, 11],
+                [12, 13, 14, 15, 16, 17],
+            ]
+        )
+        assert torch.allclose(met_cat.values, expected_values)
+        assert torch.allclose(met_cat.offset, met1.offset)
+        assert torch.allclose(met_cat.offset, met2.offset)
+
+    # case: dim=1
+    tensor_list1 = [
+        torch.tensor([[0, 1, 2], [10, 11, 12]]),
+        torch.tensor([[3, 4], [13, 14]]),
+        torch.tensor([[5], [15]]),
+    ]
+    met1 = MultiEmbeddingTensor.from_tensor_list(tensor_list1)
+    tensor_list2 = [
+        torch.tensor([[6, 7, 8, 9], [16, 17, 18, 19]]),
+    ]
+    met2 = MultiEmbeddingTensor.from_tensor_list(tensor_list2)
+    for xs in [[met1, met2], (met1, met2)]:
+        met_cat = MultiEmbeddingTensor.cat(xs, dim=1)
+        assert met_cat.num_rows == met1.num_rows
+        assert met_cat.num_rows == met2.num_rows
+        assert met_cat.num_cols == met1.num_cols + met2.num_cols
+        expected_values = torch.tensor(
+            [
+                [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+                [10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
+            ]
+        )
+        assert torch.allclose(met_cat.values, expected_values)
+        expected_offset = torch.tensor([0, 3, 5, 6, 10])
+        assert torch.allclose(met_cat.offset, expected_offset)
