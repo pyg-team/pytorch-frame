52a05208aa0787dfe883d1b1f062f916c62bd108
rusty1s <matthias.fey@tu-dortmund.de>
2023-11-24 08:36:50 +0000

    update

--- torch_frame/utils/infer_stype.py
+++ torch_frame/utils/infer_stype.py
@@ -147,5 +147,7 @@ def infer_df_stype(df: DataFrame) -> Dict[str, stype]:
     """
     col_to_stype = {}
     for col in df.columns:
-        col_to_stype[col] = infer_series_stype(df[col])
+        stype = infer_series_stype(df[col])
+        if stype is not None:
+            col_to_stype[col] = stype
     return col_to_stype
