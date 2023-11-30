c863f66ceff64f6c70743068afa869bdf585967c
Weihua Hu <weihua916@gmail.com>
2023-11-29 20:17:47 -0800

    fix

--- torch_frame/nn/encoding/positional_encoding.py
+++ torch_frame/nn/encoding/positional_encoding.py
@@ -1,4 +1,3 @@
-import numpy as np
 import torch
 from torch import Tensor
 from torch.nn import Module
@@ -19,7 +18,7 @@ class PositionalEncoding(Module):
             raise ValueError(
                 f"out_size should be divisible by 2 (got {out_size}).")
         self.out_size = out_size
-        mult_term = torch.exp(-np.log(10000.0) *
+        mult_term = torch.exp(-torch.log(10000.0) *
                               torch.arange(0, self.out_size // 2) / out_size)
         self.register_buffer("mult_term", mult_term)
 
