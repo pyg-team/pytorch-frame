38958acc187384a15b68774c11d47abedd535969
yiweny <yyuanlisette@gmail.com>
2023-11-30 06:20:40 +0000

    fix flakyness of test

--- test/nn/encoding/test_cyclic_encoding.py
+++ test/nn/encoding/test_cyclic_encoding.py
@@ -18,9 +18,10 @@ def test_cyclic_encoding_values():
         input_tensor = torch.randint(0, 2, size=size)
         cyclic_encoding = CyclicEncoding(out_size)
         out_tensor = cyclic_encoding(input_tensor)
-        assert torch.allclose(out_tensor[..., :4],
-                              torch.zeros_like(out_tensor[..., :4]), atol=1e-5)
-        assert torch.all(torch.abs(out_tensor[..., 4:] - 1) < 1e-5)
+        assert torch.allclose(
+            out_tensor[..., :4],
+            torch.zeros_like(out_tensor[..., :out_size // 2]), atol=1e-5)
+        assert torch.all(torch.abs(out_tensor[..., out_size // 2:] - 1) < 1e-5)
         assert out_tensor.shape == input_tensor.shape + (out_size, )
 
         input_zeros_tensor = torch.zeros(size)
