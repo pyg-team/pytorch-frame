1d13936a965c1ee97103c47dbe3e5d28ab1ccc7c
Weihua Hu <weihua916@gmail.com>
2023-11-29 20:16:37 -0800

    minor

--- test/nn/encoding/test_positional_encoding.py
+++ test/nn/encoding/test_positional_encoding.py
@@ -4,9 +4,9 @@ from torch_frame.nn import PositionalEncoding
 
 
 def test_positional_encoding():
-    encoding_size = 8
+    out_size = 8
     for size in [(10, ), (10, 4), (10, 5, 8)]:
         input_tensor = torch.randint(0, 10, size=size)
-        positional_encoding = PositionalEncoding(encoding_size)
+        positional_encoding = PositionalEncoding(out_size)
         out_tensor = positional_encoding(input_tensor)
-        assert out_tensor.shape == input_tensor.shape + (encoding_size, )
+        assert out_tensor.shape == input_tensor.shape + (out_size, )
