cc18eba6cec296599691d5670a54592636afcdc8
xnuohz <xnuohz@126.com>
2023-12-01 21:21:32 +0800

    update

--- torch_frame/gbdt/__init__.py
+++ torch_frame/gbdt/__init__.py
@@ -4,4 +4,9 @@ from .tuned_xgboost import XGBoost
 from .tuned_catboost import CatBoost
 from .tuned_lightgbm import LightGBM
 
-__all__ = classes = ['GBDT', 'XGBoost', 'CatBoost', 'LightGBM']
+__all__ = classes = [
+    'GBDT',
+    'XGBoost',
+    'CatBoost',
+    'LightGBM',
+]
