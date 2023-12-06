83ce61820a1d1f03b8ddb5d4a683881ded5d558f
xnuohz <xnuohz@126.com>
2023-12-06 20:20:17 +0800

    update

--- test/gbdt/test_gbdt.py
+++ test/gbdt/test_gbdt.py
@@ -3,6 +3,7 @@ import os.path as osp
 import tempfile
 
 import pytest
+import torch
 
 from torch_frame import Metric, TaskType, stype
 from torch_frame.config.text_embedder import TextEmbedderConfig
@@ -32,59 +33,12 @@ TEST_DIR = tempfile.TemporaryDirectory()
     (TaskType.BINARY_CLASSIFICATION, Metric.ROCAUC),
     (TaskType.MULTICLASS_CLASSIFICATION, Metric.ACCURACY),
 ])
-def test_gbdt(gbdt_cls, stypes, task_type_and_metric):
+def test_gbdt_with_save_load(gbdt_cls, stypes, task_type_and_metric):
     task_type, metric = task_type_and_metric
     dataset: Dataset = FakeDataset(
         num_rows=30,
         with_nan=True,
         stypes=stypes,
-        create_split=True,
-        task_type=task_type,
-        col_to_text_embedder_cfg=TextEmbedderConfig(
-            text_embedder=HashTextEmbedder(8)),
-    )
-    dataset.materialize()
-    gbdt = gbdt_cls(
-        task_type=task_type,
-        num_classes=dataset.num_classes
-        if task_type == TaskType.MULTICLASS_CLASSIFICATION else None,
-        metric=metric,
-    )
-    gbdt.tune(
-        tf_train=dataset.tensor_frame,
-        tf_val=dataset.tensor_frame,
-        num_trials=2,
-        num_boost_round=2,
-    )
-    pred = gbdt.predict(tf_test=dataset.tensor_frame)
-    score = gbdt.compute_metric(dataset.tensor_frame.y, pred)
-    assert gbdt.metric == metric
-    if task_type == TaskType.REGRESSION:
-        assert (score >= 0)
-    elif task_type == TaskType.BINARY_CLASSIFICATION:
-        assert (0 <= score <= 1)
-    elif task_type == TaskType.MULTICLASS_CLASSIFICATION:
-        assert (0 <= score <= 1)
-
-
-@pytest.mark.parametrize('gbdt_cls', [
-    CatBoost,
-    XGBoost,
-    LightGBM,
-])
-@pytest.mark.parametrize('task_type_and_metric', [
-    (TaskType.REGRESSION, Metric.RMSE),
-    (TaskType.REGRESSION, Metric.MAE),
-    (TaskType.BINARY_CLASSIFICATION, Metric.ACCURACY),
-    (TaskType.BINARY_CLASSIFICATION, Metric.ROCAUC),
-    (TaskType.MULTICLASS_CLASSIFICATION, Metric.ACCURACY),
-])
-def test_save_load_gbdt(gbdt_cls, task_type_and_metric):
-    task_type, metric = task_type_and_metric
-    dataset: Dataset = FakeDataset(
-        num_rows=30,
-        with_nan=True,
-        stypes=[stype.numerical, stype.numerical, stype.text_embedded],
         create_split=True,
         task_type=task_type,
         col_to_text_embedder_cfg=TextEmbedderConfig(
@@ -110,11 +64,15 @@ def test_save_load_gbdt(gbdt_cls, task_type_and_metric):
         num_boost_round=2,
     )
     gbdt.save(path)
-
     copy_gbdt.load(path)
-    pred = copy_gbdt.predict(tf_test=dataset.tensor_frame)
-    score = copy_gbdt.compute_metric(dataset.tensor_frame.y, pred)
-    assert copy_gbdt.metric == metric
+    pred = gbdt.predict(tf_test=dataset.tensor_frame)
+    score = gbdt.compute_metric(dataset.tensor_frame.y, pred)
+
+    copy_pred = copy_gbdt.predict(tf_test=dataset.tensor_frame)
+    copy_score = copy_gbdt.compute_metric(dataset.tensor_frame.y, pred)
+    assert torch.allclose(pred, copy_pred, atol=1e-2)
+    assert gbdt.metric == metric
+    assert score == copy_score
     if task_type == TaskType.REGRESSION:
         assert (score >= 0)
     elif task_type == TaskType.BINARY_CLASSIFICATION:
