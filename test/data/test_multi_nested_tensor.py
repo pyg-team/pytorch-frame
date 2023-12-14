fix nan masking

test/data/test_multi_nested_tensor.py
@@ -83,19 +83,33 @@ def test_multi_nested_tensor_basics(device):
 
     multi_nested_tensor_with_nan = multi_nested_tensor.clone()
     multi_nested_tensor_with_nan.values[0::2] = -1
+
     # Test fill_col
     multi_nested_tensor_1 = multi_nested_tensor_with_nan.clone()
     for col in range(multi_nested_tensor_with_nan.num_cols):
-        multi_nested_tensor_1.fill_col(col, 100)
+        multi_nested_tensor_1.fillna_col(col, 100)
     assert not torch.all(multi_nested_tensor_1.values == -1).any()
 
     # Test fillna
-    multi_nested_tensor_2 = multi_nested_tensor_with_nan
-    multi_nested_tensor_2.fillna(100)
+    multi_nested_tensor_2 = multi_nested_tensor_with_nan.clone()
+    multi_nested_tensor_2.fillna_(100)
     assert not torch.all(multi_nested_tensor_2.values == -1).any()
+
+    # Test fillna with vector of shape (num_cols)
+    multi_nested_tensor_3 = multi_nested_tensor_with_nan.clone()
+    multi_nested_tensor_3.fillna_(torch.arange(100, 110))
+    assert torch.all(
+        torch.eq(multi_nested_tensor_3.offset, multi_nested_tensor.offset))
+
+    # Test fillna with vector of shape (num_cols)
+    multi_nested_tensor_4 = multi_nested_tensor_with_nan
+    multi_nested_tensor_4.fillna_(torch.arange(100, 110).reshape(10, 1))
+    assert torch.all(
+        torch.eq(multi_nested_tensor_4.offset, multi_nested_tensor.offset))
 
     # Test eq
     assert multi_nested_tensor_1 == multi_nested_tensor_2
+    assert multi_nested_tensor_3 == multi_nested_tensor_4
 
     # Test multi_nested_tensor[i, j] indexing
     for i in range(-num_rows, num_rows):

test/nn/encoder/test_stype_encoder.py
@@ -2,16 +2,13 @@ import copy
 
 import pytest
 import torch
-from torch import Tensor
 from torch.nn import ReLU
 
 import torch_frame
-from torch_frame import NAStrategy, TensorData, stype
+from torch_frame import NAStrategy, stype
 from torch_frame.config import ModelConfig
 from torch_frame.config.text_tokenizer import TextTokenizerConfig
 from torch_frame.data.dataset import Dataset
-from torch_frame.data.multi_embedding_tensor import MultiEmbeddingTensor
-from torch_frame.data.multi_nested_tensor import MultiNestedTensor
 from torch_frame.data.stats import StatType
 from torch_frame.datasets import FakeDataset
 from torch_frame.nn import (
@@ -30,23 +27,7 @@ from torch_frame.testing.text_tokenizer import (
     RandomTextModel,
     WhiteSpaceHashTokenizer,
 )
-
-
-def eq(td1: TensorData, td2: TensorData):
-    if isinstance(td1, Tensor):
-        return isinstance(td2, Tensor) and torch.all(
-            torch.eq(td1, td2) | torch.isnan(td1) & torch.isnan(td2))
-    elif isinstance(td1, MultiNestedTensor):
-        return isinstance(td2, MultiNestedTensor) and td1 == td2
-    elif isinstance(td1, MultiEmbeddingTensor):
-        return isinstance(td2, MultiEmbeddingTensor) and td1 == td2
-    elif isinstance(td1, dict):
-        if not isinstance(td2, dict):
-            return False
-        for key in td1:
-            if not (key in td2 and td1[key] == td2[key]):
-                return False
-        return True
+from torch_frame.utils import eq
 
 
 @pytest.mark.parametrize("encoder_cls_kwargs", [(EmbeddingEncoder, {})])

torch_frame/data/multi_embedding_tensor.py
@@ -208,10 +208,10 @@ class MultiEmbeddingTensor(_MultiTensor):
             )
         assert False, "Should not reach here."
 
-    def _fill_col(
+    def _fillna_col(
         self,
         index: int,
-        fill_value: Union[float, int],
+        fill_value: Union[int, float],
     ) -> None:
         value_index = slice(self.offset[index], self.offset[index + 1])
         values = self.values[:, value_index]
@@ -219,7 +219,6 @@ class MultiEmbeddingTensor(_MultiTensor):
             values[torch.isnan(values)] = fill_value
         else:
             values[values == -1] = fill_value
-        self.values[:, value_index] = values
 
     def _empty(self, dim: int) -> MultiEmbeddingTensor:
         """Creates an empty :class:`MultiEmbeddingTensor`.

torch_frame/data/multi_nested_tensor.py
@@ -282,10 +282,10 @@ class MultiNestedTensor(_MultiTensor):
         else:
             raise RuntimeError(f"Unsupported dim={dim} for index_select.")
 
-    def _fill_col(
+    def _fillna_col(
         self,
         index: int,
-        fill_value: Union[float, int],
+        fill_value: Union[int, float],
     ) -> None:
         start_idx = torch.arange(
             index,

torch_frame/data/multi_tensor.py
@@ -325,35 +325,51 @@ class _MultiTensor:
     def _single_index_select(self, index: int, dim: int) -> _MultiTensor:
         raise NotImplementedError
 
-    def fillna(self, fill_value: Union[float, int]) -> None:
-        is_float = self.dtype.is_floating_point
-        if (is_float and isinstance(fill_value, int)
-                or not is_float and isinstance(fill_value, float)):
-            raise ValueError(
-                f"Fill value {fill_value}'s dtype does not match {self.dtype}")
-        if is_float:
+    def fillna_(self, fill_value: Union[int, float, Tensor]):
+        if isinstance(fill_value, Tensor):
+            if fill_value.dim() != 0 and not (
+                    fill_value.size(0) == self.num_cols
+                    and fill_value.numel() == self.num_cols):
+                raise ValueError(
+                    "fillna_ with Tensor of shape "
+                    f"{fill_value.shape} is not supported. "
+                    f"Please use a Tensor of shape ({self.num_cols})"
+                    f" or ({self.num_cols, 1}).")
+            if fill_value.dim() != 0:
+                for col in range(self.num_cols):
+                    self.fillna_col(col, fill_value[col])
+                return
+        if self.dtype.is_floating_point:
             self.values[torch.isnan(self.values)] = fill_value
         else:
             self.values[self.values == -1] = fill_value
 
-    def fill_col(
+    def fillna_col(
         self,
         index: int,
-        fill_value: Union[float, int],
-    ) -> None:
+        fill_value: Union[int, float, Tensor],
+    ):
         """Fill the :obj:`index`-th column in :obj:`MultiTensor` with fill_val.
 
         Args:
             index (int): A column index of the tensor to select.
             fill_value: Any
         """
-        self._fill_col(index, fill_value)
+        if isinstance(fill_value, Tensor):
+            if fill_value.numel() != 1:
+                raise ValueError(
+                    'fillna_col only supports scalars, but got Tensor '
+                    f'of shape {fill_value.shape}.')
+            else:
+                self._fillna_col(index, fill_value.item())
+        else:
+            self._fillna_col(index, fill_value)
 
-    def _fill_col(
+    def _fillna_col(
         self,
         index: int,
-        fill_value: Union[float, int],
-    ) -> None:
+        fill_value: Union[int, float],
+    ):
         raise NotImplementedError
 
 