4fdd758c981a0f7ca59c8454fdfd4ea31c0c0df8
Zecheng Zhang <zecheng@cs.stanford.edu>
2023-12-16 21:12:28 -0800

    Update

--- examples/huggingface_dataset.py
+++ examples/huggingface_dataset.py
@@ -1,3 +1,10 @@
+# This is an example to apply PyTorch Frame tabular learning
+# with text embedders on the Hugging Face Spotify Tracks Dataset directly.
+# You can specify different target columns such as "track_genre"
+# (multiclass classification) or "loudness" (regression) etc.
+# More dataset information please refer to:
+# https://huggingface.co/datasets/maharshipandya/spotify-tracks-dataset
+
 from __future__ import annotations
 
 import argparse
@@ -24,6 +31,11 @@ from torch_frame.nn import (
     LinearEncoder,
 )
 
+# Text Embedded with all-distilroberta-v1
+# ================ track_genre ================
+# Best Val Acc:  0.5169, Best Test Acc:  0.5130
+# ================= loudness ==================
+# Best Val RMSE: 2.1866, Best Test RMSE: 2.1577
 
 class TextToEmbedding:
     def __init__(self, model: str, pooling: str, device: torch.device):
@@ -63,18 +75,17 @@ def mean_pooling(last_hidden_state: Tensor, attention_mask) -> Tensor:
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--dataset", type=str,
-                    default="maharshipandya/spotify-tracks-dataset")
 parser.add_argument("--channels", type=int, default=256)
 parser.add_argument("--num_layers", type=int, default=4)
 parser.add_argument("--batch_size", type=int, default=512)
 parser.add_argument("--lr", type=float, default=0.0001)
 parser.add_argument("--epochs", type=int, default=100)
 parser.add_argument("--seed", type=int, default=0)
+parser.add_argument("--target_col", type=str, default="track_genre")
 parser.add_argument(
     "--model",
     type=str,
-    default="distilbert-base-uncased",
+    default="sentence-transformers/all-distilroberta-v1",
     choices=[
         "distilbert-base-uncased",
         "sentence-transformers/all-distilroberta-v1",
@@ -88,9 +99,11 @@ args = parser.parse_args()
 torch.manual_seed(args.seed)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+dataset = "maharshipandya/spotify-tracks-dataset"
+
 # Prepare datasets
 path = osp.join(osp.dirname(osp.realpath(__file__)), "..", "data",
-                args.dataset.replace("/", "_"))
+                dataset.replace("/", "_"))
 os.makedirs(path, exist_ok=True)
 
 col_to_stype = {
@@ -114,19 +127,18 @@ col_to_stype = {
     "time_signature": torch_frame.categorical,
     "track_genre": torch_frame.categorical,
 }
-target_col = "track_genre"
 text_embedder = TextToEmbedding(model=args.model, pooling=args.pooling,
                                 device=device)
 col_to_text_embedder_cfg = TextEmbedderConfig(text_embedder=text_embedder,
                                               batch_size=10)
 dataset = HuggingFaceDatasetDict(
-    path=args.dataset,
+    path=dataset,
     col_to_stype=col_to_stype,
-    target_col=target_col,
+    target_col=args.target_col,
     col_to_text_embedder_cfg=col_to_text_embedder_cfg,
 )
 model_name = args.model.replace('/', '')
-filename = f"{model_name}_data.pt"
+filename = f"{model_name}_{args.target_col}_data.pt"
 dataset.materialize(path=osp.join(path, filename))
 
 dataset = dataset.shuffle()
@@ -148,9 +160,16 @@ stype_encoder_dict = {
     stype.embedding: LinearEmbeddingEncoder(),
 }
 
+is_classification = dataset.task_type.is_classification
+
+if is_classification:
+    output_channels = dataset.num_classes
+else:
+    output_channels = 1
+
 model = FTTransformer(
     channels=args.channels,
-    out_channels=dataset.num_classes,
+    out_channels=output_channels,
     num_layers=args.num_layers,
     col_stats=dataset.col_stats,
     col_names_dict=train_tensor_frame.col_names_dict,
@@ -167,7 +186,10 @@ def train(epoch: int) -> float:
     for tf in tqdm(train_loader, desc=f"Epoch: {epoch}"):
         tf = tf.to(device)
         pred = model(tf)
-        loss = F.cross_entropy(pred, tf.y)
+        if is_classification:
+            loss = F.cross_entropy(pred, tf.y)
+        else:
+            loss = F.mse_loss(pred.view(-1), tf.y.view(-1))
         optimizer.zero_grad()
         loss.backward()
         loss_accum += float(loss) * len(tf.y)
@@ -184,25 +206,42 @@ def test(loader: DataLoader) -> float:
     for tf in loader:
         tf = tf.to(device)
         pred = model(tf)
-        pred_class = pred.argmax(dim=-1)
-        accum += float((tf.y == pred_class).sum())
+        if is_classification:
+            pred_class = pred.argmax(dim=-1)
+            accum += float((tf.y == pred_class).sum())
+        else:
+            accum += float(
+                F.mse_loss(pred.view(-1), tf.y.view(-1), reduction="sum"))
         total_count += len(tf.y)
 
-    accuracy = accum / total_count
-    return accuracy
+    if is_classification:
+        accuracy = accum / total_count
+        return accuracy
+    else:
+        rmse = (accum / total_count)**0.5
+        return rmse
 
-
-metric = "Acc"
-best_val_metric = 0
-best_test_metric = 0
+if is_classification:
+    metric = "Acc"
+    best_val_metric = 0
+    best_test_metric = 0
+else:
+    metric = "RMSE"
+    best_val_metric = float("inf")
+    best_test_metric = float("inf")
 
 for epoch in range(1, args.epochs + 1):
     train_loss = train(epoch)
     train_metric = test(train_loader)
     val_metric = test(val_loader)
     test_metric = test(test_loader)
-    best_val_metric = val_metric
-    best_test_metric = test_metric
+
+    if is_classification and val_metric > best_val_metric:
+        best_val_metric = val_metric
+        best_test_metric = test_metric
+    elif not is_classification and val_metric < best_val_metric:
+        best_val_metric = val_metric
+        best_test_metric = test_metric
 
     print(f"Train Loss: {train_loss:.4f}, Train {metric}: {train_metric:.4f}, "
           f"Val {metric}: {val_metric:.4f}, Test {metric}: {test_metric:.4f}")
