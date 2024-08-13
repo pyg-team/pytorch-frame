pyg-team/pytorch-frame

commit 9290633bd2c0b12d01d36bebad44d1551d406661

fix nan mask in multicategorical (#307)
Fixes #301
Will create a separate pr to improve the efficiency and remove implicit
nans.

torch_frame/nn/encoder/stype_encoder.py
@@ -174,7 +174,6 @@ class StypeEncoder(Module, ABC):
         """
         if self.na_strategy is None:
             return feat
-
         for col in range(feat.size(1)):
             column_data = feat[:, col]
             if isinstance(feat, _MultiTensor):
@@ -208,7 +207,20 @@ class StypeEncoder(Module, ABC):
                     feat.device)
             else:
                 raise ValueError(f"Unsupported NA strategy {self.na_strategy}")
-            column_data[nan_mask] = fill_value
+            if isinstance(feat, _MultiTensor):
+                feat.fillna_col(col, fill_value)
+            else:
+                column_data[nan_mask] = fill_value
+        # Add better safeguard here to make sure nans are actually
+        # replaced, expecially when nans are represented as -1's. They are
+        # very hard to catch as they won't error out.
+        filled_values = feat
+        if isinstance(feat, _MultiTensor):
+            filled_values = feat.values
+        if filled_values.is_floating_point():
+            assert not torch.isnan(filled_values).any()
+        else:
+            assert not (filled_values == -1).any()
         return feat
 
 

test/data/test_multi_embedding_tensor.py
@@ -79,6 +79,52 @@ def test_size():
     assert met.shape[2] == -1
 
 
+def test_fillna_col():
+    # Creat a MultiEmbeddingTensor containing all -1's
+    # In MultiEmbeddingTensor with torch.long dtype,
+    # -1's are considered as NaNs.
+    tensor_list = [
+        torch.tensor([[-1, 100, -1], [-1, 100, -1]]),
+        torch.tensor([[-1, 100], [100, -1]]),
+    ]
+    met_with_nan = MultiEmbeddingTensor.from_tensor_list(tensor_list)
+    columnwise_nan_mask = dict()
+    for col in range(met_with_nan.num_cols):
+        columnwise_nan_mask[col] = (met_with_nan[:, col].values == -1)
+
+    # Test fillna_col
+    for col in range(met_with_nan.num_cols):
+        met_with_nan.fillna_col(col, col)
+    assert not torch.all(met_with_nan.values == -1).any()
+    for col in range(met_with_nan.num_cols):
+        column = met_with_nan[:, col]
+        assert torch.all(column.values[columnwise_nan_mask[col]] == col)
+        assert torch.all(column.values[~columnwise_nan_mask[col]] == 100)
+
+    tensor_list = [
+        torch.tensor([[100., torch.nan, torch.nan],
+                      [torch.nan, 100., torch.nan]]),
+        torch.tensor([[torch.nan, 100.], [torch.nan, 100.]]),
+    ]
+    met_with_nan = MultiEmbeddingTensor.from_tensor_list(tensor_list)
+    columnwise_nan_mask = dict()
+    for col in range(met_with_nan.num_cols):
+        columnwise_nan_mask[col] = torch.isnan(met_with_nan[:, col].values)
+
+    # Test fillna_col
+    for col in range(met_with_nan.num_cols):
+        met_with_nan.fillna_col(col, col)
+    assert not torch.isnan(met_with_nan.values).any()
+    for col in range(met_with_nan.num_cols):
+        column = met_with_nan[:, col]
+        assert torch.all(
+            torch.isclose(column.values[columnwise_nan_mask[col]],
+                          torch.tensor([col], dtype=torch.float32)))
+        assert torch.all(
+            torch.isclose(column.values[~columnwise_nan_mask[col]],
+                          torch.tensor([100], dtype=torch.float32)))
+
+
 def test_from_tensor_list():
     num_rows = 2
     num_cols = 3

test/data/test_multi_nested_tensor.py
@@ -32,6 +32,60 @@ def column_select(
     return new_tensor_mat
 
 
+def test_fillna_col():
+    # Creat a MultiNestedTensor containing all -1's
+    # In MultiNestedTensor with torch.long dtype,
+    # -1's are considered as NaNs.
+    tensor_list = [
+        [torch.tensor([100, -1]),
+         torch.tensor([100, -1, -1])],
+        [torch.tensor([-1]), torch.tensor([-1, -1])],
+    ]
+    multi_nested_tensor_with_nan = MultiNestedTensor.from_tensor_mat(
+        tensor_list)
+    columnwise_nan_mask = dict()
+    for col in range(multi_nested_tensor_with_nan.num_cols):
+        columnwise_nan_mask[col] = (
+            multi_nested_tensor_with_nan[:, col].values == -1)
+
+    # Test fillna_col
+    for col in range(multi_nested_tensor_with_nan.num_cols):
+        multi_nested_tensor_with_nan.fillna_col(col, col)
+    assert not torch.all(multi_nested_tensor_with_nan.values == -1).any()
+    for col in range(multi_nested_tensor_with_nan.num_cols):
+        column = multi_nested_tensor_with_nan[:, col]
+        assert torch.all(column.values[columnwise_nan_mask[col]] == col)
+        assert torch.all(column.values[~columnwise_nan_mask[col]] == 100)
+
+    tensor_list = [
+        [
+            torch.tensor([100., torch.nan]),
+            torch.tensor([torch.nan, 100., torch.nan])
+        ],
+        [torch.tensor([torch.nan]),
+         torch.tensor([torch.nan, 100.])],
+    ]
+    multi_nested_tensor_with_nan = MultiNestedTensor.from_tensor_mat(
+        tensor_list)
+    columnwise_nan_mask = dict()
+    for col in range(multi_nested_tensor_with_nan.num_cols):
+        columnwise_nan_mask[col] = torch.isnan(
+            multi_nested_tensor_with_nan[:, col].values)
+
+    # Test fillna_col
+    for col in range(multi_nested_tensor_with_nan.num_cols):
+        multi_nested_tensor_with_nan.fillna_col(col, float(col))
+    assert not torch.isnan(multi_nested_tensor_with_nan.values).any()
+    for col in range(multi_nested_tensor_with_nan.num_cols):
+        column = multi_nested_tensor_with_nan[:, col]
+        assert torch.all(
+            torch.isclose(column.values[columnwise_nan_mask[col]],
+                          torch.tensor([col], dtype=torch.float32)))
+        assert torch.all(
+            torch.isclose(column.values[~columnwise_nan_mask[col]],
+                          torch.tensor([100], dtype=torch.float32)))
+
+
 @withCUDA
 def test_multi_nested_tensor_basics(device):
     num_rows = 8
@@ -220,7 +274,7 @@ def test_multi_nested_tensor_basics(device):
         MultiNestedTensor.cat([], dim=1)
 
     # Testing set item
-    with pytest.raises(RuntimeError, match="read-only"):
+    with pytest.raises(RuntimeError, match="not currently supported"):
         multi_nested_tensor[0, 0] = torch.zeros(3)
 
     # Testing clone

torch_frame/data/multi_embedding_tensor.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Sequence
+from typing import Sequence, Union
 
 import torch
 from torch import Tensor
@@ -200,6 +200,19 @@ class MultiEmbeddingTensor(_MultiTensor):
                 offset=offset,
             )
         assert False, "Should not reach here."
+
+    def fillna_col(
+        self,
+        col_index: int,
+        fill_value: Union[int, float, Tensor],
+    ) -> None:
+        values_index = slice(self.offset[col_index],
+                             self.offset[col_index + 1])
+        values_col = self.values[:, values_index]
+        if self.values.is_floating_point():
+            values_col[torch.isnan(values_col)] = fill_value
+        else:
+            values_col[values_col == -1] = fill_value
 
     def _empty(self, dim: int) -> MultiEmbeddingTensor:
         """Creates an empty :class:`MultiEmbeddingTensor`.

torch_frame/data/multi_nested_tensor.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Sequence, cast
+from typing import Sequence, Union, cast
 
 import torch
 from torch import Tensor
@@ -274,6 +274,28 @@ class MultiNestedTensor(_MultiTensor):
                                      values=values, offset=offset)
         else:
             raise RuntimeError(f"Unsupported dim={dim} for index_select.")
+
+    def fillna_col(
+        self,
+        col_index: int,
+        fill_value: Union[int, float, Tensor],
+    ) -> None:
+        start_idx = torch.arange(
+            col_index,
+            self.num_rows * self.num_cols,
+            self.num_cols,
+            device=self.device,
+        )
+        diff = self.offset[start_idx + 1] - self.offset[start_idx]
+        batch, arange = _batched_arange(diff)
+        # Compute values
+        values_index = self.offset[start_idx][batch] + arange
+        values_col = self.values[values_index]
+        if self.values.is_floating_point():
+            values_col[torch.isnan(values_col)] = fill_value
+        else:
+            values_col[values_col == -1] = fill_value
+        self.values[values_index] = values_col
 
     def to_dense(self, fill_value: int | float) -> Tensor:
         """Map MultiNestedTensor into dense Tensor representation with padding.

torch_frame/data/multi_tensor.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import copy
-from typing import Any, Callable, Sequence, TypeVar
+from typing import Any, Callable, Sequence, TypeVar, Union
 
 import torch
 from torch import Tensor
@@ -38,9 +38,8 @@ class _MultiTensor:
         }
 
     def __setitem__(self, index: Any, values: Any) -> None:
-        raise RuntimeError(
-            f"{self.__class__.__name__} object does not support setting "
-            "values. It should be used for read-only.")
+        raise RuntimeError("Setting values is not currently supported by "
+                           f"{self.__class__.__name__}.")
 
     def __repr__(self) -> str:
         return " ".join([
@@ -323,6 +322,21 @@ class _MultiTensor:
         assert False, "Should not reach here."
 
     def _single_index_select(self, index: int, dim: int) -> _MultiTensor:
+        raise NotImplementedError
+
+    def fillna_col(
+        self,
+        col_index: int,
+        fill_value: Union[int, float, Tensor],
+    ):
+        """Fill the :obj:`index`-th column in :obj:`MultiTensor` with
+        fill_value in-place.
+
+        Args:
+            col_index (int): A column index of the tensor to select.
+            fill_value (Union[int, float, Tensor]): Scalar values to replace
+                NaNs.
+        """
         raise NotImplementedError
 
 

commit a0f4d77630eb25bf41dbe748e9b2813835cb6a64

Don't put assertions on N/A imputation correctness (#433)
I understand the intention of the assertion introduced in #307, however,
we should not check it at runtime as it causes a D2H sync. It should
rather be covered in tests, and I believe there's no need to add new
tests as existing test cases already cover the correctness of the N/A
imputation.

torch_frame/nn/encoder/stype_encoder.py
@@ -250,16 +250,7 @@ class StypeEncoder(Module, ABC):
                 fill_values = torch.tensor(fill_values, device=feat.device)
                 assert feat.size(-1) == fill_values.size(-1)
                 feat = torch.where(na_mask, fill_values, feat)
-        # Add better safeguard here to make sure nans are actually
-        # replaced, expecially when nans are represented as -1's. They are
-        # very hard to catch as they won't error out.
-        filled_values = feat
-        if isinstance(feat, _MultiTensor):
-            filled_values = feat.values
-        if filled_values.is_floating_point():
-            assert not torch.isnan(filled_values).any()
-        else:
-            assert not (filled_values == -1).any()
+
         return feat
 
 