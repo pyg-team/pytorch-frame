6a0bf8cabececf2aec9537f2c9103300f03cc862
Akihiro Nitta <nitta@akihironitta.com>
2025-02-09 20:33:59 +0900

    Add support for PyTorch 2.6 (#494)
    Adds support for PyTorch 2.6.
    
    Fixes type issues raised by mypy with PyTorch 2.6:
    ```
    Found 6 errors in 4 files (checked 87 source files)
    torch_frame/nn/encoding/cyclic_encoding.py:31: error: "Tensor" not callable  [operator]
    torch_frame/nn/conv/excelformer_conv.py:95: error: Value of type "Union[Tensor, Module]" is not indexable  [index]
    torch_frame/nn/conv/excelformer_conv.py:96: error: Value of type "Union[Tensor, Module]" is not indexable  [index]
    torch_frame/nn/models/tabnet.py:261: error: "Tensor" not callable  [operator]
    torch_frame/nn/models/tabnet.py:263: error: "Tensor" not callable  [operator]
    ```

--- torch_frame/nn/encoding/positional_encoding.py
+++ torch_frame/nn/encoding/positional_encoding.py
@@ -18,9 +18,14 @@ class PositionalEncoding(Module):
             raise ValueError(
                 f"out_size should be divisible by 2 (got {out_size}).")
         self.out_size = out_size
-        mult_term = torch.pow(1 / 10000.0,
-                              torch.arange(0, self.out_size, 2) / out_size)
-        self.register_buffer("mult_term", mult_term)
+        self.mult_term: Tensor
+        self.register_buffer(
+            "mult_term",
+            torch.pow(
+                1 / 10000.0,
+                torch.arange(0, self.out_size, 2) / out_size,
+            ),
+        )
 
     def forward(self, input_tensor: Tensor) -> Tensor:
         assert torch.all(input_tensor >= 0)
