e757858f6f182f9d4eb1b0c4b4d832814ec49325
Akihiro Nitta <nitta@akihironitta.com>
2025-02-16 12:28:38 +0000

    update

--- torch_frame/datasets/__init__.py
+++ torch_frame/datasets/__init__.py
@@ -1,42 +1,44 @@
 # flake8: noqa
-
-from .titanic import Titanic
 from .adult_census_income import AdultCensusIncome
-from .fake import FakeDataset
-from .forest_cover_type import ForestCoverType
-from .mushroom import Mushroom
-from .poker_hand import PokerHand
+from .amazon_fine_food_reviews import AmazonFineFoodReviews
+from .amphibians import Amphibians
 from .bank_marketing import BankMarketing
-from .tabular_benchmark import TabularBenchmark
-from .yandex import Yandex
-from .dota2 import Dota2
-from .kdd_census_income import KDDCensusIncome
-from .multimodal_text_benchmark import MultimodalTextBenchmark
 from .data_frame_benchmark import DataFrameBenchmark
 from .data_frame_text_benchmark import DataFrameTextBenchmark
+from .diamond_images import DiamondImages
+from .dota2 import Dota2
+from .fake import FakeDataset
+from .forest_cover_type import ForestCoverType
+from .huggingface_dataset import HuggingFaceDatasetDict
+from .kdd_census_income import KDDCensusIncome
 from .mercari import Mercari
 from .movielens_1m import Movielens1M
-from .amazon_fine_food_reviews import AmazonFineFoodReviews
-from .diamond_images import DiamondImages
-from .huggingface_dataset import HuggingFaceDatasetDict
+from .multimodal_text_benchmark import MultimodalTextBenchmark
+from .mushroom import Mushroom
+from .poker_hand import PokerHand
+from .tabular_benchmark import TabularBenchmark
+from .titanic import Titanic
+from .yandex import Yandex
 
 real_world_datasets = [
-    'Titanic',
     'AdultCensusIncome',
-    'ForestCoverType',
-    'Dota2',
-    'Mushroom',
-    'PokerHand',
+    'AmazonFineFoodReviews',
+    'Amphibians',
     'BankMarketing',
-    'TabularBenchmark',
-    'Yandex',
-    'KDDCensusIncome',
-    'MultimodalTextBenchmark',
     'DataFrameBenchmark',
     'DataFrameTextBenchmark',
+    'Dota2',
+    'Titanic',
+    'ForestCoverType',
+    'HuggingFaceDatasetDict',
+    'KDDCensusIncome',
     'Mercari',
     'Movielens1M',
-    'AmazonFineFoodReviews',
+    'MultimodalTextBenchmark',
+    'Mushroom',
+    'PokerHand',
+    'TabularBenchmark',
+    'Yandex',
     'DiamondImages',
 ]
 
