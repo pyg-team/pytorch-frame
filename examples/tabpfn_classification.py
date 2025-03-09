bafc272693e1bfb7c32805420d1952ee4ec495fc
Zecheng Zhang <zecheng@alumni.stanford.edu>
2025-03-09 23:03:02 +0000

    TabPFN add column encoder

--- examples/tabpfn_classification.py
+++ examples/tabpfn_classification.py
@@ -7,12 +7,20 @@ from tabpfn import TabPFNClassifier
 # Please run `pip install tabpfn` to install the package
 from tqdm import tqdm
 
+from torch_frame import stype
 from torch_frame.data import DataLoader
 from torch_frame.datasets import (
     ForestCoverType,
     KDDCensusIncome,
     Mushroom,
     Titanic,
+)
+from torch_frame.nn import (
+    EmbeddingEncoder,
+    LinearBucketEncoder,
+    LinearEncoder,
+    LinearPeriodicEncoder,
+    StypeWiseFeatureEncoder,
 )
 
 parser = argparse.ArgumentParser(
@@ -23,6 +31,13 @@ parser.add_argument(
 parser.add_argument('--train_batch_size', type=int, default=4096)
 parser.add_argument('--test_batch_size', type=int, default=128)
 parser.add_argument('--seed', type=int, default=0)
+parser.add_argument('--out_channels', type=int, default=16)
+parser.add_argument(
+    '--numerical_encoder_type',
+    type=str,
+    default='linear',
+    choices=['linear', 'linearbucket', 'linearperiodic'],
+)
 args = parser.parse_args()
 
 torch.manual_seed(args.seed)
@@ -41,8 +56,33 @@ else:
     dataset = Mushroom(root=path)
 
 dataset.materialize()
+
 assert dataset.task_type.is_classification
 dataset = dataset.shuffle()
+
+if args.numerical_encoder_type == 'linear':
+    numerical_encoder = LinearEncoder()
+elif args.numerical_encoder_type == 'linearbucket':
+    numerical_encoder = LinearBucketEncoder()
+elif args.numerical_encoder_type == 'linearperiodic':
+    numerical_encoder = LinearPeriodicEncoder()
+else:
+    raise ValueError(
+        f'Unsupported encoder type: {args.numerical_encoder_type}')
+
+stype_encoder_dict = {
+    stype.categorical: EmbeddingEncoder(),
+    stype.numerical: numerical_encoder,
+}
+
+encoder = StypeWiseFeatureEncoder(
+    out_channels=args.out_channels,
+    col_stats=dataset.col_stats,
+    col_names_dict=dataset.tensor_frame.col_names_dict,
+    stype_encoder_dict=stype_encoder_dict,
+)
+encoder.eval()
+
 train_dataset, test_dataset = dataset[:0.9], dataset[0.9:]
 train_tensor_frame = train_dataset.tensor_frame
 test_tensor_frame = test_dataset.tensor_frame
@@ -51,11 +91,9 @@ train_loader = DataLoader(
     batch_size=args.train_batch_size,
     shuffle=True,
 )
-X_train = []
 train_data = next(iter(train_loader))
-for stype in train_data.stypes:
-    X_train.append(train_data.feat_dict[stype])
-X_train: torch.Tensor = torch.cat(X_train, dim=1)
+X_train, col_names = encoder(train_data)
+X_train = X_train.reshape(X_train.size(0), -1).detach()
 clf = TabPFNClassifier()
 clf.fit(X_train, train_data.y)
 test_loader = DataLoader(test_tensor_frame, batch_size=args.test_batch_size)
@@ -65,10 +103,8 @@ test_loader = DataLoader(test_tensor_frame, batch_size=args.test_batch_size)
 def test() -> float:
     accum = total_count = 0
     for test_data in tqdm(test_loader):
-        X_test = []
-        for stype in train_data.stypes:
-            X_test.append(test_data.feat_dict[stype])
-        X_test = torch.cat(X_test, dim=1)
+        X_test, _ = encoder(test_data)
+        X_test = X_test.reshape(X_test.size(0), -1).detach()
         pred: np.ndarray = clf.predict_proba(X_test)
         pred_class = pred.argmax(axis=-1)
         accum += float((test_data.y.numpy() == pred_class).sum())
